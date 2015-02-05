5cdb815b9ff6ba0d6ae5d23acb4acace94d18695
james7132 <james.siqi.liu@gmail.com>
2015-02-04 23:39:10 -0500

    Base Commit from Dropbox

---
+++ Assets/_scripts/PlayerInputScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerPhysics))]
+public class PlayerInputScript : MonoBehaviour {
+
+	public float walkspeed = 10;
+	public float acceleration = 8;
+	public float gravity = 20;
+	public float jumpHeight = 20;
+
+	private float currentSpeed;
+	private float targetSpeed;
+	private Vector2 amountToOffset;
+
+	private PlayerPhysics playerPhysics;
+
+	void Start ()
+	{
+		playerPhysics = GetComponent<PlayerPhysics> ();
+	}
+
+	void Update ()
+	{
+		float horizontal = Input.GetAxisRaw ("Horizontal");
+		float vertical = Input.GetAxisRaw ("Vertical");
+
+		targetSpeed = horizontal * walkspeed;
+		currentSpeed = IncrementTowards (currentSpeed, targetSpeed, acceleration);
+
+		amountToOffset.x = currentSpeed;
+		if (playerPhysics.standing) {
+			amountToOffset.y = 0;
+			if (vertical>0) {
+				amountToOffset.y = jumpHeight;
+			}
+			if (vertical<0){
+				playerPhysics.dropDown();
+			}
+		}
+		amountToOffset.y -= gravity * Time.deltaTime;
+
+		playerPhysics.move (amountToOffset * Time.deltaTime);
+	}
+
+	//increse the provided value towards the desired one
+	private float IncrementTowards(float arg, float target, float rate)
+	{
+		if (arg == target) {
+			return arg;
+		} else {
+			float dir = Mathf.Sign(target - arg);
+			arg += rate * Time.deltaTime * dir;
+			return(dir == Mathf.Sign(target - arg))? arg: target;
+		}
+	}
+
+}
---
+++ Assets/_scripts/PlayerPhysics.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPhysics : MonoBehaviour {
+
+	public LayerMask collisionMask;
+
+	private BoxCollider colider;
+	private Vector3 size;
+	private Vector3 centre;
+
+	private float skin = .005f;
+
+	public bool standing;
+	private bool dropdown;
+
+	Ray ray;
+	RaycastHit hit;
+
+	void Start()
+	{
+		colider = GetComponent<BoxCollider>();
+		size = colider.size;
+		centre = colider.center;
+		dropdown = false;
+	}
+
+	public void move(Vector2 movement)
+	{
+		float deltaY = movement.y;
+		float deltaX = movement.x;
+		Vector2 p = transform.position;
+
+		for (int i =0; i<3; ++i) {
+			float dir = Mathf.Sign(deltaY);
+			float x = (p.x + centre.x - size.x/2) + size.x/2 * i;
+			float y = p.y + centre.y + size.y/2 * dir;
+
+			ray = new Ray(new Vector2(x,y), new Vector2(0,dir));
+			Debug.DrawRay(ray.origin,ray.direction);
+					
+			if(dir<0 & Physics.Raycast(ray,out hit,Mathf.Abs(deltaY),collisionMask))
+			{
+				standing = true;
+				float dst = Vector3.Distance (ray.origin, hit.point);
+					
+				if(dst>skin)
+				{
+					deltaY = dst*dir + skin;
+				} else {
+					deltaY = 0;
+				}
+				break;
+			} else {
+				standing = false;
+			}
+		}
+
+		if (dropdown) {
+			dropdown = false;
+			deltaY -= 0.05f;
+		}
+
+		transform.Translate (new Vector3(deltaX,deltaY,0));
+	}
+
+	//Drop the guy down the ramp
+	public void dropDown()
+	{
+		dropdown = true;
+	}
+}
