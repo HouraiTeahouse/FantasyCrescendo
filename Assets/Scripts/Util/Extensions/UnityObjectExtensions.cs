a24e9a777ca75525b3db9f21d81704ec2b84fa7f
james7132 <james.siqi.liu@gmail.com>
2015-07-05 16:58:33 -0400

    Instantiate Extension methods
    Added instantiate extension methods to general Unity Objects, also
    simplified the Destroy extension methods, and added safeguards against
    deleting assets.

--- Assets/Scripts/Util/Extensions/UnityObjectExtensions.cs
+++ Assets/Scripts/Util/Extensions/UnityObjectExtensions.cs
@@ -4,26 +4,43 @@ using System.Collections.Generic;
 public static class UnityObjectExtensions {
 
     /// <summary>
+    /// Instantiates a copy of the Unity object.
+    /// Shorthand for Object.Instantiate
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="targetObject"></param>
+    /// <returns></returns>
+    public static T Instantiate<T>(this T targetObject) where T : Object {
+        return Object.Instantiate(targetObject);
+    }
+
+    public static T Instantiate<T>(this T targetObject, Vector3 position) where T : Object {
+        return Object.Instantiate(targetObject, position, Quaternion.identity) as T;
+    }
+
+    public static T Instantiate<T>(this T targetObject, Vector3 position, Quaternion rotation) where T : Object {
+        return Object.Instantiate(targetObject, position, rotation) as T;
+    }
+
+    /// <summary>
     /// Destroys the Unity object.
     /// Shorthand for Object.Destroy.
     /// If targetObject has already been destroyed, this function does nothing.
     /// </summary>
     /// <param name="targetObject"></param>
-    public static void Destroy(this Object targetObject) {
-        if(targetObject)
-            Object.Destroy(targetObject);
-    }
+    /// <param name="allowDestroyingAssets"></param>
+    public static void Destroy(this Object targetObject, bool allowDestroyingAssets = false) {
+        if (targetObject == null)
+            return;
 
-    /// <summary>
-    /// Destroys the Unity object immediately.
-    /// Use only in Editor scripts. Use in actual games is not advised.
-    /// Shorthand for Object.Destroy.
-    /// If targetObject has already been destroyed, this function does nothing.
-    /// </summary>
-    /// <param name="targetObject"></param>
-    public static void DestroyImmediate(this Object targetObject) {
-        if(targetObject)
+        if (Application.isPlaying) {
+            if (targetObject is GameObject) {
+                ((GameObject) targetObject).transform.parent = null;
+            }
             Object.Destroy(targetObject);
+        } else {
+            Object.DestroyImmediate(targetObject, allowDestroyingAssets);
+        }
     }
 
     /// <summary>
@@ -31,7 +48,8 @@ public static class UnityObjectExtensions {
     /// Does nothingi f the collection is null.
     /// </summary>
     /// <param name="targetObjects"></param>
-    public static void DestroyAll(this IEnumerable<Object> targetObjects) {
+    /// <param name="allowDestroyingAssets"></param>
+    public static void DestroyAll(this IEnumerable<Object> targetObjects, bool allowDestroyingAssets = false) {
         if (targetObjects == null)
             return;
 
@@ -42,25 +60,6 @@ public static class UnityObjectExtensions {
         else
             foreach(Object target in targetObjects)
                 target.Destroy();
-    }
-
-    /// <summary>
-    /// Destroys all of the Unity objects in a collection immediately.
-    /// Use only in Editor scripts. Use in actual games is not advised.
-    /// Does nothing if the collection is null.
-    /// </summary>
-    /// <param name="targetObjects"></param>
-    public static void DestroyAllImmedate(this IEnumerable<Object> targetObjects) {
-        if (targetObjects == null)
-            return;
-
-        Object[] arrayTest = targetObjects as Object[];
-        if (arrayTest != null)
-            for (var i = 0; i < arrayTest.Length; i++)
-                arrayTest[i].DestroyImmediate();
-        else
-            foreach (Object target in targetObjects)
-                target.DestroyImmediate();
     }
 
 }
