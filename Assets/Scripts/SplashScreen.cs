edbafb8049cdede01b18905f51a9c5b27b06e671
james7132 <james.siqi.liu@gmail.com>
2015-07-06 04:51:14 -0400

    Added Splash Screen Animation

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SplashScreen : MonoBehaviour {
+
+    [SerializeField]
+    private Image logo;
+
+    [SerializeField]
+    private AnimationCurve alphaOverTime;
+
+    [SerializeField]
+    private GameObject[] disableWhileLoading;
+
+    [SerializeField]
+    private string targetSceneName;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(DisplaySplashScreen());
+	}
+
+    IEnumerator DisplaySplashScreen() {
+        foreach (GameObject target in disableWhileLoading) {
+            target.SetActive(false);
+        }
+        if (logo != null) {
+            //Do the splash screen animation
+            float t = 0;
+            float logoDisplayDuration = alphaOverTime.keys[alphaOverTime.length - 1].time;
+            Color baseColor = logo.color;
+            Color targetColor = baseColor;
+            baseColor.a = 0f;
+            while (t < logoDisplayDuration) {
+                logo.color = Color.Lerp(baseColor, targetColor, alphaOverTime.Evaluate(t));
+                //Wait one frame
+                yield return null;
+                t += Time.deltaTime;
+            }
+        }
+        AsyncOperation operation = Application.LoadLevelAsync(targetSceneName);
+        if (operation != null && !operation.isDone) {
+            foreach (GameObject target in disableWhileLoading) {
+                target.SetActive(true);
+            }
+            while (!operation.isDone) {
+                yield return null;
+            }
+        }
+        Destroy(gameObject);
+    }
+}
