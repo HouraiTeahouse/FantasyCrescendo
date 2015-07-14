f5878ae2129859be2b1ab67ea65eee1b50b04065
james7132 <james.siqi.liu@gmail.com>
2015-07-13 20:17:22 -0400

    Singleton Fix
    There was a problem accessing Singletons in the Editor. Added a fix to
    allievate the problem. All Singleton accesses should now work as long as
    one instance is in the scene at edit time.

--- Assets/API/Util/Singleton.cs
+++ Assets/API/Util/Singleton.cs
@@ -9,8 +9,12 @@ public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T> {
 
     public static T Instance {
         get {
-            if (_instance == null)
-                Debug.LogError("Something is trying to access the " + typeof(T) +  " Singleton instance, but none exists.");
+            if (_instance == null) {
+                _instance = FindObjectOfType<T>();
+                if (_instance == null) {
+                    Debug.LogError("Something is trying to access the " + typeof(T) + " Singleton instance, but none exists.");      
+                }
+            }
             return _instance;
         }
     }
