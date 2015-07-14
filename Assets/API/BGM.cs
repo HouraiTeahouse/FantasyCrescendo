6e7702948ffb51c790706bf2a9a9da3f05a6bfc0
james7132 <james.siqi.liu@gmail.com>
2015-07-13 23:34:34 -0400

    Added BGM to Test Stage
    Added BGM class to add static access to the background music.
    Moved Directional Light to Stage Requirements Prefab to ensure that every
    scene has consistent lighting.

---
+++ Assets/API/BGM.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Genso.API {
+
+    [RequireComponent(typeof(AudioSource))]
+    public class BGM : Singleton<BGM> {
+
+        private static AudioSource bgmSource;
+
+        public static AudioClip CurrentlyPlaying {
+            get { return bgmSource.clip; }
+        }
+
+        public static void Play(AudioClip bgm) {
+            bgmSource.Stop();
+            bgmSource.clip = bgm;
+            bgmSource.Play();
+        }
+
+        public static void Stop() {
+            bgmSource.Stop();
+        }
+
+        protected override void Awake() {
+            base.Awake();
+            bgmSource = GetComponent<AudioSource>();
+        }
+
+    }
+
+}
