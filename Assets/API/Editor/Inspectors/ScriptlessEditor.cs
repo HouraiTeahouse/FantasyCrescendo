f773c35e00a7e8cdc2bab6892d8c7f0ae250bb23
james7132 <james.siqi.liu@gmail.com>
2015-08-07 15:52:38 -0400

    Removed the "Script" field from all Editors
    It's always there, it's excessive and unnecessary. Create universal,
    fallback editors for all derivatives of MonoBehaviour and
    ScriptableObject that voids the script field.

---
+++ Assets/API/Editor/Inspectors/ScriptlessEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Crescendo.API.Editor{
+    
+    /// <summary>
+    /// Removes the extra "Script" field on any editor derived from this.
+    /// </summary>
+    public abstract class ScriptlessEditor : UnityEditor.Editor {
+
+        public override void OnInspectorGUI() {
+            SerializedProperty iterator = serializedObject.GetIterator();
+            iterator.Next(true);
+            while (iterator.NextVisible(false)) {
+                if (iterator.name != "m_Script")
+                    EditorGUILayout.PropertyField(iterator, true);
+            }
+        }
+
+    }
+
+    [CustomEditor(typeof(MonoBehaviour), true, isFallback = true)]
+    internal sealed class MonoBehaviourEditor : ScriptlessEditor {
+    }
+
+    [CustomEditor(typeof(ScriptableObject), true, isFallback = true)]
+    internal sealed class ScriptableObjectEditor : ScriptlessEditor {
+    }
+
+}
