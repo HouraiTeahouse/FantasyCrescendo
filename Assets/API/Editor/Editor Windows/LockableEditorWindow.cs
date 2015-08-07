6f849b7025b871d36c2c593086f411e0b739ebfc
james7132 <james.siqi.liu@gmail.com>
2015-08-07 16:08:06 -0400

    Added Abstract EditorWindow classes
    LockableEditorWindow creates a EditorWindow with the same padlock button
    that the Inspector and various built in windows have.
    
    SelectableEditorWindow acts like the Inspector in that it can lock its
    selection using the padlock.

---
+++ Assets/API/Editor/Editor Windows/LockableEditorWindow.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+using Object = UnityEngine.Object;
+
+namespace Crescendo.API.Editor {
+
+    /// <summary>
+    /// Creates a EditorWindow that has the small padlock button at the top
+    /// like the Inspector.
+    /// 
+    /// The state of the padlock can be accessed through the Locked property.
+    /// </summary>
+    public abstract class LockableEditorWindow : EditorWindow, IHasCustomMenu {
+
+        [System.NonSerialized]
+        GUIStyle lockButtonStyle;
+
+        [System.NonSerialized]
+        bool locked = false;
+
+        public bool IsLocked {
+            get { return locked; }
+            set { locked = value; }
+        }
+
+        void ShowButton(Rect position) {
+            if (lockButtonStyle == null)
+                lockButtonStyle = "IN LockButton";
+            locked = GUI.Toggle(position, locked, GUIContent.none, lockButtonStyle);
+        }
+        
+        void IHasCustomMenu.AddItemsToMenu(GenericMenu menu) {
+            menu.AddItem(new GUIContent("Lock"), locked, () => {
+                locked = !locked;
+            });
+        }
+    }
+}
