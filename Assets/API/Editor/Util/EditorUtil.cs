71df4959ddbd8412a2642c0d5d194da897ee3a54
james7132 <james.siqi.liu@gmail.com>
2015-08-07 16:20:41 -0400

    Folder Reorganization for Editor Scripts

---
+++ Assets/API/Editor/Util/EditorUtil.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using UnityEditor;
+using UnityObject = UnityEngine.Object;
+
+namespace Crescendo.API.Editor {
+    
+    public static class EditorUtil {
+
+/*       
+        This somehow generates a compiler error. Commenting it out for now.
+
+        public class HorizontalArea : IDisposable {
+
+            public HorizontalArea(GUIStyle style, GUILayoutOption[] options) {
+                if (options == null) {
+                    if (style == null) {
+                        EditorGUILayout.BeginHorizontal();
+                    } else {
+                        EditorGUILayout.BeginHorizontal(style);
+                    }
+                } else {
+                    if (style == null) {
+                        EditorGUILayout.BeginHorizontal(options);
+                    } else {
+                        EditorGUILayout.BeginHorizontal(style, options);
+                    }
+                }
+            }
+            
+            public void Dispose() {
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+
+        public static IDisposable Horizontal(GUIStyle style = null, params GUILayoutOption[] options) {
+            return new HorizontalArea(style, options);
+        }*/
+
+        /// <summary>
+        /// Since GameObject.GetComponentInChildren does not work on deactivated objects, which include 
+        /// prefabs in the Editor. This method is a workaround for finding Components on children of
+        /// prefabs.
+        /// 
+        /// This method also works with interfaces.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="prefab"></param>
+        /// <returns></returns>
+        public static T GetComponentInChildren<T>(GameObject prefab) where T : class {
+            if(prefab == null)
+                throw new ArgumentNullException("prefab");
+            foreach (var child in prefab.Children()) {
+                T instance = child.GetIComponent<T>();
+                if (instance != null)
+                    return instance;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Since GameObject.GetComponentsInChildren does not work on deactivated objects, which include 
+        /// prefabs in the Editor. This method is a workaround for finding Components on children of
+        /// prefabs.
+        /// 
+        /// This method also works with interfaces.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="prefab"></param>
+        /// <returns></returns>
+        public static T[] GetComponentsInChildren<T>(GameObject prefab) where T : class {
+            if (prefab == null)
+                throw new ArgumentNullException("prefab");
+            List<T> instances = new List<T>();
+            foreach (var child in prefab.Children()) {
+                Debug.Log(child);
+                instances.AddRange(child.GetIComponents<T>());
+            }
+            return instances.ToArray();
+        }
+
+        public static T GetComponentInChildren<T>(Component prefab) where T : class{
+            if(prefab == null)
+                throw new ArgumentNullException("prefab");
+            return GetComponentInChildren<T>(prefab.gameObject);
+        }
+
+        public static T[] GetComponentsInChildren<T>(Component prefab) where T : class {
+            if (prefab == null)
+                throw new ArgumentNullException("prefab");
+            return GetComponentsInChildren<T>(prefab.gameObject);
+        }
+
+        public static string Text(string label, string text) {
+            return EditorGUILayout.TextField(text, label);
+        }
+
+        public static void Space(int? space = null) {
+            if(space == null)
+                GUILayout.FlexibleSpace();
+            else
+                GUILayout.Space((int)space);
+        }
+
+    }
+
+}
