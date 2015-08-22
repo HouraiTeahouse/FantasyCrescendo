600ae489242d1da05e55ac621b822056d42496ff
james7132 <james.siqi.liu@gmail.com>
2015-08-21 20:35:44 -0400

    Added Attack Type and Direction for distinction

--- Assets/Dependencies/SmashBrew/Fight/Attack.cs
+++ Assets/Dependencies/SmashBrew/Fight/Attack.cs
@@ -1,6 +1,37 @@
-﻿namespace Hourai.SmashBrew {
+﻿using UnityEngine;
+
+namespace Hourai.SmashBrew {
+    
+    public enum Direction {
+        Neutral = 0,
+        Forward,
+        Backward,
+        Up,
+        Down
+    }
+
+    public enum AttackType {
+        Ground = 0,
+        Smash,
+        Special,
+        Aerial
+    }
 
     public class Attack : CharacterAnimationBehaviour {
+
+        [SerializeField]
+        private Direction _direction;
+
+        [SerializeField]
+        private AttackType _type;
+
+        public Direction Direction {
+            get { return _direction; }
+        }
+
+        public AttackType Type {
+            get { return _type; }
+        }
 
     }
 
