f297fa3fac67bc8d988a07f2e82a3e66d43618c5
james7132 <james.siqi.liu@gmail.com>
2015-11-29 14:47:21 -0500

    Color/Material Swaps with Resources
    Reduces number of redundant prefabs created. Each character simply has one
    prefab now.

---
+++ Assets/Dependencies/SmashBrew/Character/MaterialSwap.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Hourai.SmashBrew {
+
+    [RequireComponent(typeof(Renderer))]
+    public class MaterialSwap : MonoBehaviour {
+
+        [SerializeField, Resource(typeof(Material))]
+        private string[] _alternativeMaterials;
+
+        void Start() {
+            var playerController = GetComponentInParent<PlayerController>();
+            if (playerController != null) {
+                if (playerController.PlayerData == null)
+                    Debug.LogError("PlayerController does not have a Player", playerController);
+                else {
+                    int palletSwap = playerController.PlayerData.Pallete;
+                    if(palletSwap >= 0 && palletSwap < _alternativeMaterials.Length)
+                        GetComponent<Renderer>().material = Resources.Load<Material>(_alternativeMaterials[palletSwap]);
+                }
+            }
+            Destroy(this);
+        }
+
+    }
+
+} 
