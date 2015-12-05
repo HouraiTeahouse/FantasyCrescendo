ceec7ca8ee098f85fbfc7a3ee34d29e1f7d3c1df
james7132 <james.siqi.liu@gmail.com>
2015-12-05 00:26:02 -0500

    Match Timer GUI
    Finally made the timer at the top of the GUI functional.
    
    Hides itself when the match isn't a Timed Match, and updates itself every
    second.

---
+++ Assets/Dependencies/SmashBrew/UI/MatchTimerGUI.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Hourai.SmashBrew.UI {
+
+    [RequireComponent(typeof(Text))]
+    public class MatchTimerGUI : MonoBehaviour {
+
+        private Text _displayText;
+        private TimeMatch _timeMatch;
+        private int currentDisplayedTime;
+
+        void Start() {
+            _displayText = GetComponent<Text>();
+            _timeMatch = FindObjectOfType<TimeMatch>();
+            enabled = _timeMatch && _timeMatch.isActiveAndEnabled;
+            _displayText.enabled = enabled;
+        }
+
+        void Update() {
+            var remainingTime = (int) _timeMatch.CurrentTime;
+            if (remainingTime == currentDisplayedTime)
+                return;
+            currentDisplayedTime = remainingTime;
+            int seconds = remainingTime % 60;
+            int minutes = remainingTime / 60;
+            _displayText.text = string.Format("{0}:{1}", minutes.ToString("d2"), seconds.ToString("d2"));
+        }
+
+    }
+
+}
