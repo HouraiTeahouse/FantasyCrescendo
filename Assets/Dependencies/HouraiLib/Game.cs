99bd4152fa1c4cb8b63f332b619414b0b3c18631
james7132 <james.siqi.liu@gmail.com>
2015-12-04 19:10:01 -0500

    Basic Pausing
    Only triggered by pressing escape. Will expand to multiple players later.

--- Assets/Dependencies/HouraiLib/Game.cs
+++ Assets/Dependencies/HouraiLib/Game.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections;
-using System.Linq;
 using UnityConstants;
 using UnityEngine;
 
---
+++ Assets/PauseGame.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Hourai.Events;
+
+namespace Hourai.SmashBrew {
+
+    public class PauseEvent : IEvent {
+
+
+
+    }
+
+    public class PauseGame : MonoBehaviour {
+
+        [SerializeField]
+        private KeyCode _key = KeyCode.Escape;
+
+        void Update() {
+            if (Input.GetKeyDown(_key))
+                Game.Paused = !Game.Paused;
+        }
+
+    }
+
+}
