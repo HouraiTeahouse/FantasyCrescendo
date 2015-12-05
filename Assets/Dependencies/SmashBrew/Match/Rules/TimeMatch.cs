6e31b9f349a12820ff2368e9fe0156f5ef2901a7
james7132 <james.siqi.liu@gmail.com>
2015-12-05 00:21:07 -0500

    Timed Matches
    No winning criteria yet. Just a timer to end the match.

---
+++ Assets/Dependencies/SmashBrew/Match/Rules/TimeMatch.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using Hourai.Events;
+
+namespace Hourai.SmashBrew {
+
+    public class TimeMatch : MatchRule {
+
+        [SerializeField]
+        private float _time = 180f;
+
+        public float CurrentTime { get; private set; }
+
+        protected override bool IsFinished {
+            get { return isActiveAndEnabled && CurrentTime <= 0; }
+        }
+
+        public override Character Winner {
+            get { return null; }
+        }
+
+        private Mediator _eventManager;
+
+        protected override void Awake() {
+            base.Awake();
+            _eventManager = GlobalEventManager.Instance;
+            _eventManager.Subscribe<Match.MatchEvent>(OnMatchStart);
+        }
+
+        void OnMatchStart(Match.MatchEvent eventArgs) {
+            if (!eventArgs.start)
+                return;
+            CurrentTime = _time;
+        }
+
+        protected override void Update() {
+            base.Update();
+            CurrentTime -= Time.deltaTime;
+        }
+
+    }
+
+}
