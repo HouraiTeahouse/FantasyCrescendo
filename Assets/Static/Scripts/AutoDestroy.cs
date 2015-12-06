20923f423fdc24cd7db2a182c269912a61162fcc
james7132 <james.siqi.liu@gmail.com>
2015-12-06 02:23:40 -0500

    Generalized Auto Destroy Script

---
+++ Assets/Static/Scripts/AutoDestroy.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Hourai {
+
+    public class AutoDestroy : MonoBehaviour {
+
+        private Animation _animation;
+        private AudioSource _audio;
+        private ParticleSystem _particleSystem;
+
+        void Awake() {
+            _animation = GetComponent<Animation>();
+            _audio = GetComponent<AudioSource>();
+            _particleSystem = GetComponent<ParticleSystem>();
+        }
+
+        // Update is called once per frame
+        void Update() {
+            if (_animation && _animation.isPlaying)
+                return;
+            if (_audio && _audio.isPlaying)
+                return;
+            if (_particleSystem && _particleSystem.isPlaying)
+                return;
+            Destroy(gameObject);
+        }
+
+    }
+
+}
