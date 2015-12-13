698799b2a2df1fe2857ef2196b4c34a9d04f3ca0
james7132 <james.siqi.liu@gmail.com>
2015-12-13 18:07:32 -0500

    Pallete Swap Code Refactor
    Is now a singular component required on every Character prefab's root, and
    not on a per-renderer script. This prevents materials from being loaded
    multiple times by different renderers.
    
    TODO: Create a better editor for it.

---
+++ Assets/Dependencies/SmashBrew/Character/PalleteSwap.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Hourai.SmashBrew {
+
+    [RequiredCharacterComponent]
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(PlayerController))]
+    public class PalleteSwap : MonoBehaviour {
+
+        [System.Serializable]
+        private class Swap {
+
+            [Resource(typeof(Material))]
+            public string[] AlternativeMaterials;
+            public Renderer[] TargetRenderers;
+
+        }
+
+        [SerializeField]
+        private Swap[] _swaps;
+
+        void Start() {
+            var playerController = GetComponentInParent<PlayerController>();
+            if (playerController.PlayerData == null)
+                Debug.LogError("PlayerController does not have a Player", playerController);
+            else { 
+                int palletSwap = playerController.PlayerData.Pallete;
+                foreach (var swap in _swaps) {
+                    if (palletSwap < 0 || palletSwap >= swap.AlternativeMaterials.Length)
+                        continue;
+                    var material = Resources.Load<Material>(swap.AlternativeMaterials[palletSwap]);
+                    if (material == null)
+                        continue;
+                    foreach(Renderer rend in swap.TargetRenderers)
+                        if (rend)
+                            rend.sharedMaterial = material;
+                }
+            }
+        }
+
+    }
+
+} 
