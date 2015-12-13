8fc74f8c3ceaced995efe0d4510475cb4761564e
james7132 <james.siqi.liu@gmail.com>
2015-12-13 18:32:03 -0500

    Added Documenation and Tooltips for SceneData

--- Assets/Dependencies/SmashBrew/SceneData.cs
+++ Assets/Dependencies/SmashBrew/SceneData.cs
@@ -5,22 +5,40 @@ namespace Hourai.SmashBrew {
 
     public class SceneData : BGMGroup {
 
+        /// <summary>
+        /// The internal name of the scene. Must be in build settings. 
+        /// </summary>
         [SerializeField]
+        [Tooltip("The internal name of the scene. Must be in build settings.")]
         private string _sceneName;
 
         [SerializeField]
+        [Tooltip("Is this scene a stage?")]
         private bool _isStage = true;
 
-        [SerializeField, Resource(typeof (Sprite))]
+        [SerializeField]
+        [Resource(typeof(Sprite))]
+        [Tooltip("The image shown on menus to represent the scene.")]
         private string _previewImage;
 
-        [SerializeField, Resource(typeof (Sprite))]
+        [SerializeField]
+        [Resource(typeof (Sprite))]
+        [Tooltip("The icon used shown on menus to represent the scene.")]
         private string _icon;
 
+        /// <summary>
+        /// The image shown on menus to represent the scene.
+        /// </summary>
         public Resource<Sprite> PreviewImage { get; private set; }
 
+        /// <summary>
+        /// The icon used on menus to represent the scene.
+        /// </summary>
         public Resource<Sprite> Icon { get; private set; }
 
+        /// <summary>
+        /// Is the scene described by this SceneData a stage?
+        /// </summary>
         public bool IsStage {
             get { return _isStage; }
         }
