181b4cdc6080e49eb457cfbb67c33434fb934f47
james7132 <james.siqi.liu@gmail.com>
2016-01-01 13:58:16 -0500

    Hide Mouse in Matches

---
+++ Assets/HideMouse.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Hourai {
+
+    /// <summary>
+    /// Hides the cursor
+    /// </summary>
+    public class HideMouse : MonoBehaviour {
+
+        /// <summary>
+        /// Whether the cursor hiding works in the editor or not.
+        /// </summary>
+        [SerializeField]
+        private bool _inEditor;
+
+#if UNITY_EDITOR
+        void OnEnable() {
+            if(_inEditor)
+                Cursor.visible = false;
+        }
+
+        void OnDisable() {
+            if(_inEditor)
+                Cursor.visible = true;
+        }
+#else
+        void OnEnable() {
+            Cursor.visible = false;
+        }
+
+        void OnDisable() {
+            Cursor.visible = true;
+        }
+#endif
+    }
+
+}
