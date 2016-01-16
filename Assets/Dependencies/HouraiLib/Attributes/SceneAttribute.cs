cb682130d05c405ffb2eb22a90edde8d59a6aafa
james7132 <james.siqi.liu@gmail.com>
2016-01-16 12:46:27 -0500

    SceneAttribute/Drawer
    Exposed Object Field for SceneAssets for easier use in editors. Saves
    Scene path to a string field

---
+++ Assets/Dependencies/HouraiLib/Attributes/SceneAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Hourai {
+
+    /// <summary>
+    /// PropertyAttribute with a drawer that exposes a SceneAsset object field.
+    /// MUST be a string field.
+    /// Saves the path of the SceneAsset to the field.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field)]
+    public class SceneAttribute : PropertyAttribute {
+
+    }
+
+}
