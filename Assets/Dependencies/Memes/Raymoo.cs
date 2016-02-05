5d7513d8056be1797bc3aab516f20af3ab0d1c5f
James Liu <James Liu>
2016-02-04 22:27:47 -0800

    Memes:Raymoo

---
+++ Assets/Dependencies/Memes/Raymoo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Raymoo : MonoBehaviour {
+
+    public bool IsPoor
+    {
+        get { return true; }
+    }
+
+    public string ToString()
+    {
+        return "Waki Miko Re-i-mu~";
+    }
+
+}
