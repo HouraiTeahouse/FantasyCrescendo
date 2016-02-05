7a0a2bc3a2f5d297399bb350477a97a31ab176fa
James Liu <James Liu>
2016-02-04 22:26:20 -0800

    Memes:Serno

---
+++ Assets/Dependencies/Memes/Serno.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Serno
+{
+
+    public int IsBaka
+    {
+        get { return true; }
+    }
+
+    public string ToString()
+    {
+        return "⑨";
+    }
+
+}
