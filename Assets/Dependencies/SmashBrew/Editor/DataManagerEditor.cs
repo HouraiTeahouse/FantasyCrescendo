219d06a0caea000195b2cd5a53e7bab8e26e4a9d
james7132 <james.siqi.liu@gmail.com>
2016-02-19 00:22:03 -0800

    Changed Character and Scenes to ReorderableLists

--- Assets/Dependencies/SmashBrew/Editor/DataManagerEditor.cs
+++ Assets/Dependencies/SmashBrew/Editor/DataManagerEditor.cs
@@ -1,22 +1,55 @@
+using System;
 using System.Linq;
 using HouraiTeahouse.Editor;
+using HouraiTeahouse.SmashBrew;
 using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
 
 namespace HouraiTeahouse.SmashBrew.Editor {
 
+
     [CustomEditor(typeof(DataManager))]
     public class DataManagerEditor : ScriptlessEditor {
+
+        private SerializedProperty characters;
+        private SerializedProperty scenes;
+        private ReorderableList characterList;
+        private ReorderableList sceneList;
+
+        void OnEnable() {
+            characters = serializedObject.FindProperty("_characters");
+            scenes = serializedObject.FindProperty("_scenes");
+            characterList = new ReorderableList(serializedObject, characters, true, false, true, true);
+            sceneList = new ReorderableList(serializedObject, scenes, true, false, true, true);
+            InitializeList(characterList);
+            InitializeList(sceneList);
+        }
+
+        void InitializeList(ReorderableList list) {
+            list.drawElementCallback = delegate(Rect rect, int index, bool active, bool focused) {
+                rect.y += 2;
+                rect.height -= 4;
+                EditorGUI.PropertyField(rect, list.serializedProperty.GetArrayElementAtIndex(index), new GUIContent(string.Format("ID: {0}", index.ToString("D3"))));
+            };
+            list.drawHeaderCallback = delegate(Rect rect) {
+                EditorGUI.LabelField(rect, list.serializedProperty.displayName);
+            };
+            list.onAddCallback = delegate(ReorderableList reorderableList) {
+                SerializedProperty property = reorderableList.serializedProperty;
+                property.InsertArrayElementAtIndex(property.arraySize);
+            };
+        }
+
         public override void OnInspectorGUI() {
-            base.OnInspectorGUI();
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("_dontDestroyOnLoad"));
+            characterList.DoLayoutList();
+            sceneList.DoLayoutList();
             if (GUILayout.Button("Refresh")) {
-                serializedObject.FindProperty("_characters")
-                    .SetArray(Resources.LoadAll<CharacterData>(""));
-                serializedObject.FindProperty("_scenes")
-                    .SetArray(Resources.LoadAll<SceneData>(""));
+                characters.SetArray(Resources.LoadAll<CharacterData>(""));
+                scenes.SetArray(Resources.LoadAll<SceneData>(""));
                 serializedObject.ApplyModifiedProperties();
             }
-
         }
     }
 }
