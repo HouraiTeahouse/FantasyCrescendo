61e320294eef14fdd3ee748113ddd4fbb852ad19
james7132 <james.siqi.liu@gmail.com>
2016-02-22 00:16:40 -0800

    Fixed LanguageManagerEditor Issues

--- Assets/Dependencies/HouraiLocalization/Editor/LanguageManagerEditor.cs
+++ Assets/Dependencies/HouraiLocalization/Editor/LanguageManagerEditor.cs
@@ -1,38 +1,39 @@
 using System;
-using System.Globalization;
 using System.Linq;
 using System.Text.RegularExpressions;
+using HouraiTeahouse.Editor;
 using UnityEditor;
 
 namespace HouraiTeahouse.Localization.Editor {
 
-    using Editor = UnityEditor.Editor;
-
     [CustomEditor(typeof(LanguageManager))]
-    public class LanguageManagerEditor : Editor {
+    public class LanguageManagerEditor : ScriptlessEditor {
 
         private string[] availableLanguages;
+        private string[] display;
         private int _index;
 
         private Regex _splitCamelCase;
 
-        void OnEnable() {
-            _splitCamelCase = new Regex("([A-Z])");
+        protected override void OnEnable() {
+            base.OnEnable();
+            _splitCamelCase = new Regex(".([A-Z])");
         }
 
         public override void OnInspectorGUI() {
-            DrawDefaultInspector();
+            base.OnInspectorGUI();
             if (!EditorApplication.isPlayingOrWillChangePlaymode) 
                 return;
             var langManager = target as LanguageManager;
 
             if (availableLanguages == null) {
-                availableLanguages = langManager.AvailableLanguages.Select(lang => _splitCamelCase.Replace(lang, " $1")).ToArray();
-                _index = Array.LastIndexOf(availableLanguages, _splitCamelCase.Replace(langManager.CurrentLangauge.name, " $1"));
+                availableLanguages = langManager.AvailableLanguages.ToArray();
+                display = availableLanguages.Select(lang => _splitCamelCase.Replace(lang, " $1")).ToArray();
+                _index = Array.LastIndexOf(availableLanguages, langManager.CurrentLangauge.name);
             }
 
             int oldIndex = _index;
-            _index = EditorGUILayout.Popup("Current Language", _index, availableLanguages);
+            _index = EditorGUILayout.Popup("Current Language", _index, display);
             if (_index != oldIndex) {
                 langManager.LoadLanguage(availableLanguages[_index]);
             }
