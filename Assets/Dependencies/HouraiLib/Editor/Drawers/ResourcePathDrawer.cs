145303aac2b427091769b063a5901797636c20cd
james7132 <james.siqi.liu@gmail.com>
2016-02-28 01:48:15 -0800

    Improved ResourceAttribute Editors
    Previously it had a lot of trouble working with arrays, often displaying
    the wrong message while used on arrays/lists. Now it works properly with
    all lists and multi-draw instances

--- Assets/Dependencies/HouraiLib/Editor/Drawers/ResourcePathDrawer.cs
+++ Assets/Dependencies/HouraiLib/Editor/Drawers/ResourcePathDrawer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using Object = UnityEngine.Object;
@@ -9,13 +10,59 @@ namespace HouraiTeahouse.Editor {
     /// Custom PropertyDrawer for ResourcePathAttribute.
     /// </summary>
     [CustomPropertyDrawer(typeof (ResourceAttribute))]
-    public class ResourcePathEditor : PropertyDrawer {
+    internal class ResourceAttributeDrawer : PropertyDrawer {
 
-        private SerializedProperty prop;
-        private Type type;
-        private Object obj;
-        private string message;
-        private bool success;
+        private class Data {
+            private Object _object;
+            private string _path;
+            public readonly GUIContent Content;
+
+            public Data(SerializedProperty property, GUIContent content) {
+                _path = property.stringValue;
+                _object = Resources.Load(_path);
+                Content = new GUIContent(content);
+                UpdateContent(content);
+            }
+
+            public void Draw(Rect position, SerializedProperty property, Type type) {
+                EditorGUI.BeginChangeCheck();
+                Object obj = EditorGUI.ObjectField(position, Content, _object, type, false);
+                if (EditorGUI.EndChangeCheck()) {
+                    Update(obj);
+                    property.stringValue = _path;
+                    EditorUtility.SetDirty(property.serializedObject.targetObject);
+                }
+            }
+
+            public void UpdateContent(GUIContent label) {
+                bool validPath = !string.IsNullOrEmpty(_path);
+                Content.text = string.Format("{0} ({1})", label.text, validPath ? "\u2713" : "\u2715");
+                string message;
+                if (!_object)
+                    message = "No object specified";
+                else if (!validPath)
+                    message = "Not in a Resources folder. Will not be saved.";
+                else
+                    message = string.Format("Path: {0}", _path);
+
+                if (string.IsNullOrEmpty(label.tooltip))
+                    Content.tooltip = message;
+                else
+                    Content.tooltip = string.Format("{0}\n{1}", label.tooltip, message);
+            }
+
+           void Update(Object obj) {
+                _object = obj;
+                _path = AssetUtil.GetResourcePath(_object);
+            }
+        }
+
+        private readonly Dictionary<string, Data> _data;
+        private ResourceAttribute _resourceAttribute;
+
+        public ResourceAttributeDrawer() {
+           _data = new Dictionary<string, Data>(); 
+        }
 
         /// <summary>
         /// <see cref="PropertyDrawer.OnGUI"/>
@@ -25,44 +72,21 @@ namespace HouraiTeahouse.Editor {
                 EditorGUI.PropertyField(position, property, label);
                 return;
             }
-            EditorGUI.BeginProperty(position, label, property);
+            if (_resourceAttribute == null)
+                _resourceAttribute = attribute as ResourceAttribute;
 
-            string path = property.stringValue;
-            bool changed = prop != property;
-            if (changed) {
-                obj = Resources.Load(path);
-                prop = property;
-            }
+            string propertyPath = property.propertyPath;
+            Data data;
+            bool changed = !_data.ContainsKey(propertyPath);
+            if (changed) 
+                data = new Data(property, label);
+            else 
+                data = _data[propertyPath];
 
-            float height = base.GetPropertyHeight(property, label);
-            position.height = height;
-
-            label.text += " (" + ((success) ? "\u2713" : "\u2715") + ")";
-            label.tooltip = message;
-
-            obj = EditorGUI.ObjectField(position, label, obj, (attribute as ResourceAttribute).TypeRestriction, false);
-
-            if (GUI.changed || changed) {
-                message = string.Empty;
-                success = false;
-                if (!obj) {
-                    path = string.Empty;
-                    message = "No Object Specified";
-                }
-                else {
-                    string resourcePath = AssetUtil.GetResourcePath(obj);
-                    if (string.IsNullOrEmpty(resourcePath)) {
-                        message = "Not in a Resources folder. Will not be saved.";
-                    }
-                    else {
-                        message = "Path: " + resourcePath;
-                        path = resourcePath;
-                        success = true;
-                    }
-                }
-            }
-            property.stringValue = path;
-
+            EditorGUI.BeginProperty(position, data.Content, property);
+            data.UpdateContent(label);
+            data.Draw(position, property, _resourceAttribute.TypeRestriction);
+            _data[propertyPath] = data;
             EditorGUI.EndProperty();
         }
 
