7330043137cd5793839281fe06e9c7c1c97210f4
james7132 <james.siqi.liu@gmail.com>
2016-03-03 19:51:03 -0800

    Added Unity Analytics and Unity Game Performance
    Added these two services as a way to measure how the game is performing
    on player computers and how players are playing the game.

---
+++ Assets/Dependencies/UnityPerf/GameCrashReporting.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.CrashLog;
+
+/// <summary>
+/// Initializes Crash Reporting using Unity Game Performance Reporting.
+/// </summary>
+public class GameCrashReporting : MonoBehaviour 
+{
+    /// <summary>
+    /// The Unity Project ID for the project
+    /// </summary>
+    [SerializeField]
+    private string _unityProjectID;
+
+    /// <summary>
+    /// Unity callback. Called on object instantiation.
+    /// </summary>
+    void Awake() {
+		CrashReporting.Init(_unityProjectID);
+    }
+
+}
