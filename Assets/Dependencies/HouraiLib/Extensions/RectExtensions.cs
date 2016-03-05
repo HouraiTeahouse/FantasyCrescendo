acb6b8dbc017db3aaeb37100e0f96a0374cf82a5
james7132 <james.siqi.liu@gmail.com>
2016-03-05 12:28:15 -0800

    Documented RectExtensions

--- Assets/Dependencies/HouraiLib/Extensions/RectExtensions.cs
+++ Assets/Dependencies/HouraiLib/Extensions/RectExtensions.cs
@@ -1,6 +1,5 @@
 using System;
 using UnityEngine;
-using UnityEngine.Assertions;
 
 namespace HouraiTeahouse {
 
@@ -9,6 +8,12 @@ namespace HouraiTeahouse {
     /// </summary>
     public static class RectExtensions {
 
+        /// <summary>
+        /// Changes the width of a Rect without changing the centered position of the Rect.
+        /// </summary>
+        /// <param name="rect">the source Rect</param>
+        /// <param name="width">the new width of the Rect</param>
+        /// <returns>the edited Rect</returns>
         public static Rect ChangeWidth(this Rect rect, float width) {
             float diff = rect.width - width;
             rect.width = width;
@@ -16,6 +21,12 @@ namespace HouraiTeahouse {
             return rect;
         }
 
+        /// <summary>
+        /// Changes the height of a Rect without changing the centered posiiton of the Rect. 
+        /// </summary>
+        /// <param name="rect">the source Rect</param>
+        /// <param name="height">the new height of the Rect</param>
+        /// <returns>the edited Rect</returns>
         public static Rect ChangeHeight(this Rect rect, float height) {
             float diff = rect.height - height;
             rect.height = height;
@@ -23,20 +34,45 @@ namespace HouraiTeahouse {
             return rect;
         }
 
+        /// <summary>
+        /// Changes both the height and width of the a Rect without changing the centered position of the Rect.
+        /// </summary>
+        /// <param name="rect">the source Rect</param>
+        /// <param name="width">the new width of the Rect</param>
+        /// <param name="height">the new height of the Rect</param>
+        /// <returns>the edited Rect</returns>
         public static Rect ChangeDims(this Rect rect, float width, float height) {
             return rect.ChangeWidth(width).ChangeHeight(height);
         }
 
+        /// <summary>
+        /// Changes both the height and width of the a Rect without changing the centered position of the Rect. 
+        /// </summary>
+        /// <param name="rect">the source Rect</param>
+        /// <param name="dims">the new dimensions of the Rect</param>
+        /// <returns>the edited Rect</returns>
         public static Rect ChangeDims(this Rect rect, Vector2 dims) {
             return rect.ChangeDims(dims.x, dims.y);
         }
 
+        /// <summary>
+        /// Gets the aspect ratio of the rect.
+        /// If the height of the Rect is zero, returns NaN.
+        /// </summary>
+        /// <param name="rect">the Rect to get the aspect ratio of</param>
+        /// <returns>the aspect ratio of the Rect</returns>
         public static float AspectRatio(this Rect rect) {
             if (rect.height == 0f)
                 return float.NaN;
             return rect.width / rect.height;
         }
 
+        /// <summary>
+        /// Enforces an aspect ratio on a Rect. Will expand the Rect if necessary
+        /// </summary>
+        /// <param name="rect">the source Rect</param>
+        /// <param name="aspect">the aspect ratio to enforce</param>
+        /// <returns>the edited Rect</returns>
         public static Rect EnforceAspect(this Rect rect, float aspect) {
             if (rect.AspectRatio() < aspect) {
                 // Image is wider than cropRect, extend it sideways
@@ -46,6 +82,27 @@ namespace HouraiTeahouse {
             return rect.ChangeHeight(rect.width / aspect);
         }
 
+        /// <summary>
+        /// Restricts a Rect to to a certain size. Retains the center.
+        /// Optionally can enforce an aspect ratio.
+        /// </summary>
+        /// <param name="rect">the source Rect</param>>
+        /// <param name="size">the maximum dimensions of the new Rect</param>
+        /// <param name="aspect">the aspect ratio to enforce, will not enforce one if null</param>
+        /// <returns>the edited Rect</returns>
+        public static Rect Restrict(this Rect rect, Vector2 size, float? aspect = null) {
+            return rect.Restrict(size.x, size.y, aspect);
+        }
+
+        /// <summary>
+        /// Restricts a Rect to to a certain size. Retains the center.
+        /// Optionally can enforce an aspect ratio.
+        /// </summary>
+        /// <param name="rect">the source Rect</param>
+        /// <param name="width">the maximum width of the new Rect</param>
+        /// <param name="height">the maximum height of the new Rect</param>
+        /// <param name="aspect">the aspect ratio to enforce, will not enforce one if null</param>
+        /// <returns>the edited Rect</returns>
         public static Rect Restrict(this Rect rect, float width, float height, float? aspect = null) {
             Vector2 center = rect.center;
             if (aspect == null) {
