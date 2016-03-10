d351037c7a6945b7e7f62dc9f3f55e533614cb72
james7132 <james.siqi.liu@gmail.com>
2016-03-09 19:29:10 -0800

    Added Player Pointer Movement

---
+++ Assets/Dependencies/SmashBrew/UI/Menu/PlayerPointer.cs
@@ -0,0 +1,41 @@
+using InControl;
+using UnityEngine;
+
+namespace HouraiTeahouse.SmashBrew.UI {
+
+    public class PlayerPointer : PlayerUIComponent {
+
+        private RectTransform _rTransform;
+        
+        [SerializeField]
+        private float _movementSpeed = 20;
+
+        [SerializeField]
+        private InputControlTarget _horizontal = InputControlTarget.LeftStickX;
+
+        [SerializeField]
+        private InputControlTarget _vertical = InputControlTarget.LeftStickY;
+
+        private InputDevice _input;
+
+        protected override void OnPlayerChange() {
+            base.OnPlayerChange();
+            _input = Player != null ? Player.Controller : null;
+        }
+
+        protected override void Awake() {
+            base.Awake();
+            _rTransform = transform as RectTransform;
+        }
+
+        // Update is called once per frame
+        void Update() {
+            if (_input == null)
+                return;
+            Vector2 movement = new Vector2(_input.GetControl(_horizontal), _input.GetControl(_vertical));
+            Bounds bounds = new Bounds(Vector3.zero, (_rTransform.parent as RectTransform).rect.size - 0.5f * _rTransform.sizeDelta);
+            _rTransform.anchoredPosition = bounds.ClosestPoint(_rTransform.anchoredPosition + _movementSpeed * movement * Time.deltaTime);
+        }
+    }
+
+}
