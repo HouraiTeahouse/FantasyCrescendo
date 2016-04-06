530a40a68b55a1b67938a9a29551cb934dc6efee
Akshay Ahluwalia <akshayahluwalia@Akshays-Air.home>
2016-04-05 21:10:32 -0400

    Added data that will later be modified to be a stage select screen. Stage select.unity is a copy of character select SceneSelecter.cs is a copy of PlayerCharacterSelector.cs

---
+++ Assets/Dependencies/SmashBrew/UI/Menu/SceneSelector.cs
@@ -0,0 +1,9 @@
+﻿namespace HouraiTeahouse.SmashBrew.UI {
+
+    public class StageSelector : CharacterUIComponent, IPlayerClickable {
+        public void Click(Player player) {
+            if(Character && Character.IsSelectable)
+                player.SelectedCharacter = Character;
+        }
+    }
+}
