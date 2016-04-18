8a1341603ade4934f65cb8168664f1e49af74270
james7132 <james.siqi.liu@gmail.com>
2016-04-17 19:47:44 -0700

    Player Controller Mapping and Input Extensions

---
+++ Assets/Dependencies/SmashBrew/Input/InputControlExtensions.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using HouraiTeahouse.HouraiInput;
+
+namespace HouraiTeahouse.SmashBrew {
+
+    public static class InputControlExtensions {
+
+        public static IEnumerable<InputControl> GetControl(this IEnumerable<InputDevice> devices, InputTarget target) {
+            return devices.Select(d => d.GetControl(target));
+        }
+
+        public static IEnumerable<InputControl> GetControls(this InputDevice device, IEnumerable<InputTarget> targets) {
+            return targets.Select<InputTarget, InputControl>(device.GetControl);
+        }
+
+        public static IEnumerable<InputControl> GetControls(this IEnumerable<InputDevice> devices,
+            IEnumerable<InputTarget> targets) {
+            return devices.SelectMany(d => d.GetControls(targets));
+        }
+
+    }
+
+}
--- Assets/Dependencies/SmashBrew/PlayerController.cs
+++ Assets/Dependencies/SmashBrew/PlayerController.cs
@@ -2,15 +2,19 @@ using HouraiTeahouse.HouraiInput;
 using UnityEngine;
 
 namespace HouraiTeahouse.SmashBrew {
+
     [DisallowMultipleComponent]
     public class PlayerController : HouraiBehaviour {
         public Player PlayerData { get; set; }
 
         private Character _character;
+        private PlayerControlMapping _controlMapping;
 
         protected override void Awake() {
             base.Awake();
             _character = GetComponent<Character>();
+            //TODO: Generalize this 
+            _controlMapping = new PlayerControlMapping();
         }
 
         void Update() {
@@ -19,20 +23,23 @@ namespace HouraiTeahouse.SmashBrew {
 
             InputDevice input = PlayerData.Controller;
 
+            Vector2 stick = _controlMapping.Stick(input);
+            Vector2 altStick = _controlMapping.AltStick(input);
+
             //Ensure that the character is walking in the right direction
-            if ((input.LeftStickX > 0 && _character.Direction) ||
-                (input.LeftStickX < 0 && !_character.Direction))
+            if ((stick.x > 0 && _character.Direction) ||
+                (stick.x < 0 && !_character.Direction))
                 _character.Direction = !_character.Direction;
 
-            Animator.SetFloat(CharacterAnim.HorizontalInput, input.LeftStickX.Value);
-            Animator.SetFloat(CharacterAnim.VerticalInput, input.LeftStickY.Value);
-            Animator.SetBool(CharacterAnim.AttackInput, input.Action1.WasPressed);
-            Animator.SetBool(CharacterAnim.SpecialInput, input.Action2.WasPressed);
-            Animator.SetBool(CharacterAnim.ShieldInput, input.LeftTrigger || input.RightTrigger);
+            Animator.SetFloat(CharacterAnim.HorizontalInput, stick.x);
+            Animator.SetFloat(CharacterAnim.VerticalInput, stick.y);
+            Animator.SetBool(CharacterAnim.AttackInput, _controlMapping.Attack(input));
+            Animator.SetBool(CharacterAnim.SpecialInput, _controlMapping.Special(input));
+            Animator.SetBool(CharacterAnim.ShieldInput, _controlMapping.Shield(input));
 
-            // Jump on X and Y presses
-            if(input.Action3.WasPressed || input.Action4.WasPressed)
+            if(_controlMapping.Jump(input))
                 _character.Jump();
         }
     }
+
 }
