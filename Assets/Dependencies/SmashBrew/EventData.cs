d6c8311483be9353b36a40680d745dc648700751
james7132 <james.siqi.liu@gmail.com>
2016-07-25 21:35:35 -0700

    Character Portrait Art Update + Build Fix

--- Assets/Dependencies/SmashBrew/EventData.cs
+++ Assets/Dependencies/SmashBrew/EventData.cs
@@ -2,8 +2,10 @@ using System;
 using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor;
 using UnityEngine.Assertions;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace HouraiTeahouse.SmashBrew {
     [Serializable]
