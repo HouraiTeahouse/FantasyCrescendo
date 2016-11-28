d7e9b39bd301ea11c2d24ff49c6553a8601698ab
james7132 <james.siqi.liu@gmail.com>
2016-11-27 19:47:30 -0800

    Move TouhouCharacterData out of SmashBrew

--- Assets/Dependencies/SmashBrew/Character/CharacterData.cs
+++ Assets/Dependencies/SmashBrew/Character/CharacterData.cs
@@ -3,69 +3,6 @@ using UnityEngine;
 
 namespace HouraiTeahouse.SmashBrew {
 
-    public enum TouhouGame {
-
-        HighlyResponsiveToPrayers = 0,
-        StoryOfEasternWonderland,
-        PhantasmagoriaOfDimDream,
-        LotusLandStory,
-        MysticSquare,
-        EmbodimentOfScarletDevil,
-        PerfectCherryBlossom,
-        ImmaterialAndMissingPower,
-        ImperishableNight,
-        PhantasmagoriaOfFlowerView,
-        ShootTheBullet,
-        MountainOfFaith,
-        ScarletWeatherRhapsody,
-        SubterraneanAnimism,
-        UndefinedFantasticObject,
-        Hisoutensoku,
-        DoubleSpoiler,
-        FairyWars,
-        TenDesires,
-        HopelessMasquerade,
-        ImpossibleSpellCard,
-        UrbanLegendInLimbo,
-        LegacyOfLunaticKingdom,
-        Other
-
-    }
-
-    public enum TouhouStage {
-
-        PlayableCharacter = 0,
-        Stage1,
-        Stage2,
-        Stage3,
-        Stage4,
-        Stage5,
-        Stage6,
-        Extra,
-        Other
-
-    }
-
-    [Extension(typeof(CharacterData))]
-    public class TouhouCharacterData : ScriptableObject {
-
-        [SerializeField]
-        TouhouGame _sourceGame = TouhouGame.Other;
-
-        [SerializeField]
-        TouhouStage _sourceStage = TouhouStage.Other;
-
-        /// <summary> The source game the character is from </summary>
-        public TouhouGame SourceGame {
-            get { return _sourceGame; }
-        }
-
-        /// <summary> The source stage the character is from </summary>
-        public TouhouStage SourceStage {
-            get { return _sourceStage; }
-        }
-
-    }
 
     /// <summary> A ScriptableObject </summary>
     /// <seealso cref="DataManager" />
