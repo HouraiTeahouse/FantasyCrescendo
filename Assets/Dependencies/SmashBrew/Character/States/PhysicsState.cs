ce0e2353cca372a8a3b4d8b0024e1a2505240154
james7132 <james.siqi.liu@gmail.com>
2016-11-27 20:00:49 -0800

    Add public properties to PhysicsState

--- Assets/Dependencies/SmashBrew/Character/States/PhysicsState.cs
+++ Assets/Dependencies/SmashBrew/Character/States/PhysicsState.cs
@@ -23,6 +23,18 @@ namespace HouraiTeahouse.SmashBrew.Characters {
         [Tooltip("The downward velocity of the character while fast falling")]
         float _fastFallSpeed = 5f;
 
+        [SerializeField]
+        [Tooltip("The minimum walking speed of the character")]
+        float _slowWalkSpeed = 2f;
+
+        [SerializeField]
+        [Tooltip("The maximum walking speed of the character")]
+        float _fastWalkSpeed = 4f;
+
+        [SerializeField]
+        [Tooltip("The running speed of the character")]
+        float _runSpeed = 4f;
+
         // Character Variables 
         [Header("Variables")]
         [SyncVar, SerializeField, ReadOnly]
@@ -33,7 +45,48 @@ namespace HouraiTeahouse.SmashBrew.Characters {
         [Tooltip("How much shield health the character currently has")]
         Vector2 _acceleration;
 
-        public void ResetState() { throw new System.NotImplementedException(); }
+        public float Weight {
+            get { return _weight; }
+        }
+
+        public float Gravity1 {
+            get { return _gravity; }
+        }
+
+        public float MaxFallSpeed {
+            get { return _maxFallSpeed; }
+        }
+
+        public float FastFallSpeed {
+            get { return _fastFallSpeed; }
+        }
+
+        public float SlowWalkSpeed {
+            get { return _slowWalkSpeed; }
+        }
+
+        public float FastWalkSpeed {
+            get { return _fastWalkSpeed; }
+        }
+
+        public float RunSpeed {
+            get { return _runSpeed; }
+        }
+
+        public Vector2 Velocity {
+            get { return _velocity; }
+            set { _velocity = value; }
+        }
+
+        public Vector2 Acceleration {
+            get { return _acceleration; }
+            set { _acceleration = value; }
+        }
+
+        public void ResetState() {
+            Velocity = Vector2.zero;
+            Acceleration = Vector2.zero;
+        }
 
     }
 
