825c45bdcbb2d02e99cef0a140a39f6415355d71
james7132 <james.siqi.liu@gmail.com>
2016-11-29 23:21:30 -0800

    Cleanup DataManager

--- Assets/Dependencies/SmashBrew/Core/DataManager.cs
+++ Assets/Dependencies/SmashBrew/Core/DataManager.cs
@@ -9,38 +9,28 @@ namespace HouraiTeahouse.SmashBrew {
     /// <summary> A manager of all of the game data loaded into the game. </summary>
     public sealed class DataManager : MonoBehaviour {
 
-        ReadOnlyCollection<CharacterData> _characterCollection;
+        [SerializeField]
+        [Tooltip("Destroy this instance on scene loads?")]
+        bool _dontDestroyOnLoad;
 
         [SerializeField]
         [Tooltip("The characters to display in the game")]
         List<CharacterData> _characters;
 
         [SerializeField]
-        [Tooltip("Destroy this instance on scene loads?")]
-        bool _dontDestroyOnLoad;
-
-        ReadOnlyCollection<SceneData> _sceneCollection;
-
-        [SerializeField]
         [Tooltip("The scenes to show in the game")]
         List<SceneData> _scenes;
-
-        public Mediator Mediator { get; private set; }
 
         /// <summary> The Singleton instance of DataManager. </summary>
         public static DataManager Instance { get; private set; }
 
         /// <summary> All Characters that are included with the Game's build. The Data Manager will automatically load all
         /// CharacterData instances from Resources. </summary>
-        public ReadOnlyCollection<CharacterData> Characters {
-            get { return _characterCollection; }
-        }
+        public ReadOnlyCollection<CharacterData> Characters { get; private set; }
 
         /// <summary> All Scenes and their metadata included with the game's build. The DataManager will automatically load all
         /// SceneData instances from Resources. </summary>
-        public ReadOnlyCollection<SceneData> Scenes {
-            get { return _sceneCollection; }
-        }
+        public ReadOnlyCollection<SceneData> Scenes { get; private set; }
 
         /// <summary> Unity Callback. Called on object instantion. </summary>
         void Awake() {
@@ -49,13 +39,11 @@ namespace HouraiTeahouse.SmashBrew {
             if (_dontDestroyOnLoad)
                 DontDestroyOnLoad(this);
 
-            Mediator = Mediator.Global;
-
             _characters = _characters.IgnoreNulls().ToList();
             _scenes = _scenes.IgnoreNulls().ToList();
 
-            _characterCollection = new ReadOnlyCollection<CharacterData>(_characters);
-            _sceneCollection = new ReadOnlyCollection<SceneData>(_scenes);
+            Characters = new ReadOnlyCollection<CharacterData>(_characters);
+            Scenes = new ReadOnlyCollection<SceneData>(_scenes);
 
             SceneManager.sceneLoaded += SceneLoad;
         }
--- Assets/Dependencies/SmashBrew/Editor/Inspectors/DataManagerEditor.cs
+++ Assets/Dependencies/SmashBrew/Editor/Inspectors/DataManagerEditor.cs
@@ -1,6 +1,5 @@
 using HouraiTeahouse.Editor;
 using UnityEditor;
-using UnityEditorInternal;
 using UnityEngine;
 
 namespace HouraiTeahouse.SmashBrew.Editor {
@@ -11,41 +10,17 @@ namespace HouraiTeahouse.SmashBrew.Editor {
 
         SerializedProperty characters;
         SerializedProperty scenes;
-        ReorderableList characterList;
-        ReorderableList sceneList;
 
         void OnEnable() {
             characters = serializedObject.FindProperty("_characters");
             scenes = serializedObject.FindProperty("_scenes");
-            characterList = new ReorderableList(serializedObject, characters, true, false, true, true);
-            sceneList = new ReorderableList(serializedObject, scenes, true, false, true, true);
-            InitializeList(characterList);
-            InitializeList(sceneList);
-        }
-
-        void InitializeList(ReorderableList list) {
-            list.drawElementCallback = delegate(Rect rect, int index, bool active, bool focused) {
-                rect.y += 2;
-                rect.height -= 4;
-                EditorGUI.PropertyField(rect,
-                    list.serializedProperty.GetArrayElementAtIndex(index),
-                    new GUIContent(string.Format("ID: {0}", index.ToString("D3"))));
-            };
-            list.drawHeaderCallback =
-                delegate(Rect rect) { EditorGUI.LabelField(rect, list.serializedProperty.displayName); };
-            list.onAddCallback = delegate(ReorderableList reorderableList) {
-                SerializedProperty property = reorderableList.serializedProperty;
-                property.InsertArrayElementAtIndex(property.arraySize);
-            };
         }
 
         /// <summary>
         ///     <see cref="UnityEditor.Editor.OnInspectorGUI" />
         /// </summary>
         public override void OnInspectorGUI() {
-            EditorGUILayout.PropertyField(serializedObject.FindProperty("_dontDestroyOnLoad"));
-            characterList.DoLayoutList();
-            sceneList.DoLayoutList();
+            DrawDefaultInspector();
             if (GUILayout.Button("Refresh")) {
                 characters.SetArray(Resources.LoadAll<CharacterData>(""));
                 scenes.SetArray(Resources.LoadAll<SceneData>(""));
