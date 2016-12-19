0db4b56d5392da6f3dbcb05d2235514a9afc7670
james7132 <james.siqi.liu@gmail.com>
2016-12-19 00:19:16 -0800

    Remove old gravity and ground components

--- Assets/Dependencies/SmashBrew/Character/Components/Gravity.cs
+++ Assets/Dependencies/SmashBrew/Character/Components/Gravity.cs
@@ -2,14 +2,13 @@ using UnityEngine;
 
 namespace HouraiTeahouse.SmashBrew {
 
-    [RequireComponent(typeof(Ground), typeof(Rigidbody))]
+    [RequireComponent(typeof(Rigidbody))]
     public sealed class Gravity : MonoBehaviour {
 
         [SerializeField]
         [Tooltip("The acceleration downward per second applied")]
         float _gravity = 9.86f;
 
-        Ground _ground;
         Rigidbody _rigidbody;
 
         /// <summary> Gets or sets the magnitude of gravity applied to the Character. </summary>
@@ -21,15 +20,12 @@ namespace HouraiTeahouse.SmashBrew {
         public static implicit operator float(Gravity gravity) { return gravity != null ? gravity.Force : 0f; }
 
         void Awake() {
-            _ground = GetComponent<Ground>();
             _rigidbody = GetComponent<Rigidbody>();
         }
 
         void FixedUpdate() {
             float grav = Force;
             //Simulates ground friction.
-            if (_ground)
-                grav *= 2.5f;
             _rigidbody.AddForce(-Vector3.up * grav, ForceMode.Acceleration);
         }
 
