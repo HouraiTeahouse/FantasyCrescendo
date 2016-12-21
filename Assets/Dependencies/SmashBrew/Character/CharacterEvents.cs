47f8025a9601bbcc883ca5c2abf74c49b915817a
james7132 <james.siqi.liu@gmail.com>
2016-12-21 01:28:18 -0800

    Remove unused events

--- Assets/Dependencies/SmashBrew/Character/CharacterEvents.cs
+++ Assets/Dependencies/SmashBrew/Character/CharacterEvents.cs
@@ -2,25 +2,6 @@ using UnityEngine;
 
 namespace HouraiTeahouse.SmashBrew {
 
-    /// <summary> Events fired when a player jumps </summary>
-    public class PlayerJumpEvent {
-
-        // whether the player was on the Ground when jumping or not
-        public bool Ground;
-
-        // The number of remaining jumps after this one
-        public int RemainingJumps;
-
-    }
-
-    /// <summary> Events fired when a player touches the Ground or leaves the gum </summary>
-    public class PlayerGroundEvent {
-
-        // whether the player is now on the gorund or not
-        public bool Grounded;
-
-    }
-
     /// <summary> Events fired when a player dies </summary>
     public class PlayerDieEvent {
 
@@ -50,31 +31,6 @@ namespace HouraiTeahouse.SmashBrew {
 
         // the player that is to be respawned
         public Player Player;
-
-    }
-
-    /// <summary> Events fired when a player is damaged </summary>
-    public class PlayerDamageEvent {
-
-        // the final damage after being damaged
-        public float CurrentDamage;
-        // the damage dealt to the player
-        public float Damage;
-
-    }
-
-    /// <summary> Events fired when a player is healed </summary>
-    public class PlayerHealEvent {
-
-        // the final damage after healed 
-        public float CurrentDamage;
-        // the amount of damage that has been healed
-        public float Healing;
-
-    }
-
-    /// <summary> Events fired when a player is knocked back </summary>
-    public class PlayerKnockbackEvent {
 
     }
 
