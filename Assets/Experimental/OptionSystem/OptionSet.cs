903ca382d1bbce4956ed54146bb904a0592b61e9
DiligentGear <liyuan.qi1994@gmail.com>
2017-02-04 05:47:31 -0600

    Added metadata and example UI Slider Builder using current system

--- Assets/Experimental/OptionSystem/OptionSet.cs
+++ Assets/Experimental/OptionSystem/OptionSet.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace HouraiTeaHouse
+namespace HouraiTeahouse
 {
     [AttributeUsage(AttributeTargets.Class)]
     public class OptionCategory : Attribute
     {
         private string name;
-        public OptionCategory(string name)
+        public OptionCategory(string name, params string[] args)
         {
             this.name = name;
         }
@@ -38,27 +38,111 @@ namespace HouraiTeaHouse
         }
     }
 
+    public class UISlider : Option
+    {
+        float min;
+        public float Min
+        {
+            get
+            {
+                return min;
+            }
+        }
+
+        float max;
+        public float Max
+        {
+            get
+            {
+                return max;
+            }
+        }
+
+        float defValue;
+        public float DefValue
+        {
+            get
+            {
+                return defValue;
+            }
+        }
+
+        public UISlider(string name, float min, float max, float defValue = 0.0f) : base(name)
+        {
+            this.min = min;
+            this.max = max;
+        }
+    }
+
+    public class UIToggle : Option
+    {
+        bool defValue;
+        public bool DefValue
+        {
+            get
+            {
+                return defValue;
+            }
+        }
+        public UIToggle(string name, bool defValue = false) : base(name)
+        {
+            this.defValue = defValue;
+        }
+    }
+
+    public class UIIntField : Option
+    {
+        int defValue;
+        public int DefValue
+        {
+            get
+            {
+                return defValue;
+            }
+        }
+        public UIIntField(string name, int defValue = 0) : base(name)
+        {
+            this.defValue = defValue;
+        }
+    }
+
+    public class UITextField : Option
+    {
+        string defValue;
+        public string DefValue
+        {
+            get
+            {
+                return defValue;
+            }
+        }
+        public UITextField(string name, string defValue = "") : base(name)
+        {
+            this.defValue = defValue;
+        }
+    }
+
     [OptionCategory("Audio Options")]
     public class AudioOptions
     {
-        [Option("Master")]
+        [UISlider("Master Volume", 0.0f, 1.0f, 1.0f)]
         public float Master { get; set; }
-        [Option("BGM")]
+        [UISlider("Background Music", 0.0f, 1.0f, 1.0f)]
         public float Bgm { get; set; }
-        [Option("SFX")]
+        [UISlider("Sound Effects", 0.0f, 1.0f, 1.0f)]
         public float Sfx { get; set; }
     }
 
     [OptionCategory("Some Options")]
     public class SomeOptions
     {
-        [Option("Some Float")]
+        [UISlider("Some Float", -100.0f, 100.0f, 60.0f)]
         public float SomeFloat { get; set; }
-        [Option("Some Bool")]
+        [UIToggle("Some Bool", true)]
         public bool SomeBool { get; set; }
-        [Option("Some Int")]
+        [UIIntField("Some Int", 123)]
         public int SomeInt { get; set; }
-        [Option("Some String")]
+        [UITextField("Some String", "Okayama no kenboku")]
         public string SomeString { get; set; }
     }
 }
