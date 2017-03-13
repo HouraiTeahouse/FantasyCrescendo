36009b68f758066a072c2063a3ec1051c3b6bacb
james7132 <james.siqi.liu@gmail.com>
2017-03-12 19:52:31 -0700

    Added toggle button for showing hitboxes

---
+++ Assets/Source/Testing/HitobxDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HouraiTeahouse.SmashBrew;
+using UnityEngine;
+
+namespace HouraiTeahouse {
+
+public class HitobxDisplay : MonoBehaviour {
+
+    [SerializeField]
+    KeyCode _key = KeyCode.F11;
+
+    /// <summary> Unity callback. Called once per frame. </summary>
+    void Update() {
+        if (Input.GetKeyDown(_key))
+            Hitbox.DrawHitboxes = !Hitbox.DrawHitboxes;
+    }
+
+}
+
+}
+
