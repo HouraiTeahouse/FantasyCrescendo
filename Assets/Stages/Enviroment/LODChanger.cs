bb47cab0202e9794894a02841e56c69ea1259579
Gabo7 <gabriel-sierra@hotmail.com>
2017-04-03 20:56:09 -0500

    Added Quality Scaler scripts, LODs for trees and mushrooms and updated Quality Settings
    - Introducing MoonRabbit: Quality Scaler script
    - LOD setter script
    - LODs for trees and mushrooms
    - Other small fixes

---
+++ Assets/Stages/Enviroment/LODChanger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LODChanger : MonoBehaviour {
+
+    [SerializeField]
+    MeshRenderer []HighQualityLOD;
+
+    [SerializeField]
+    MeshRenderer[]LowQualityLOD;
+
+    // Use this for initialization
+    public void EnableLODs () {
+
+            foreach (var render in HighQualityLOD)
+            {
+                render.enabled = false;
+                
+            }
+            foreach (var render in LowQualityLOD)
+            {
+                
+                render.enabled = true;
+            }
+        }
+	
+
+}
