ec3a4ca9a4f22f1aa81e2f47c5189412c8d1d5ea
james7132 <james.siqi.liu@gmail.com>
2017-06-09 00:58:37 -0700

    Basic scene select screen

---
+++ Assets/Dependencies/SmashBrew/UI/Scene/SceneSelectMenuBuilder.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace HouraiTeahouse.SmashBrew.UI {
+
+    /// <summary> Constructs the maps select section of the in-match UI </summary>
+    public class SceneSelectMenuBuilder : AbstractSelectMenuBuilder<SceneData> {
+
+        [SerializeField]
+        bool _requireSelectable;
+
+        [SerializeField]
+        bool _includeMenus;
+
+        [SerializeField]
+        bool _includeStages = true;
+
+        [SerializeField]
+        bool _includeDebugScenes;
+
+        protected override IEnumerable<SceneData> GetData() { return DataManager.Scenes; }
+
+        protected override bool CanShowData(SceneData data) {
+            if (_requireSelectable && !data.IsSelectable)
+                return false;
+            if (data.Type == SceneType.Menu && !_includeMenus)
+                return false;
+            if (data.Type == SceneType.Stage && !_includeStages)
+                return false;
+            return true;
+        }
+
+    }
+
+}
