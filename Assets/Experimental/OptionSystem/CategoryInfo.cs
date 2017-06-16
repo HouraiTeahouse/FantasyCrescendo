ae627c240ba9ca164742d7a47f2eb240fa76f1ad
james7132 <james.siqi.liu@gmail.com>
2017-06-16 15:01:06 -0700

    Add event based listeners to change

---
+++ Assets/Experimental/OptionSystem/CategoryInfo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace HouraiTeahouse.Options {
+
+    public sealed class CategoryInfo {
+
+        public object Instance { get; private set; }
+        public Type Type { get; private set; }
+        public string Name { get; private set; }
+        readonly Dictionary<string, OptionInfo> _options;
+
+        public CategoryInfo(object instance) {
+            _options = new Dictionary<string, OptionInfo>();
+            Instance = instance;
+            Type = Argument.NotNull(instance).GetType();
+            var categoryAttribute = Type.GetCustomAttributes(true).OfType<OptionCategoryAttribute>().FirstOrDefault();
+            if (categoryAttribute != null) {
+                Name = categoryAttribute.Name ?? Type.Name;
+            }
+
+            foreach (var prop in Type.GetProperties()) {
+                var attribute = prop.GetCustomAttributes(true).OfType<OptionAttribute>().FirstOrDefault();
+                if (attribute != null) {
+                    Log.Debug(prop.Name);
+                    _options.Add(prop.Name, new OptionInfo(this, attribute, Type, prop));
+                }
+            }
+        }
+
+        public IEnumerable<OptionInfo> Options {
+            get { return _options.Values; }
+        }
+
+        public OptionInfo GetInfo(string name) {
+            return _options[name];
+        }
+
+    }
+
+}
