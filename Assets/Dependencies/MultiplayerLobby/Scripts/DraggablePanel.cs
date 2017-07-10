38e11ac06e6e1bfc5b1dd6faa7510f73a83f39a4
james7132 <james.siqi.liu@gmail.com>
2017-07-10 00:48:07 -0700

    Add Multiplayer Lobby UI

---
+++ Assets/Dependencies/MultiplayerLobby/Scripts/DraggablePanel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Prototype.NetworkLobby
+{
+    public class DraggablePanel : MonoBehaviour, IDragHandler
+    {
+        public void OnDrag(PointerEventData eventData)
+        {
+            transform.Translate(eventData.delta);
+        }
+    }
+}
