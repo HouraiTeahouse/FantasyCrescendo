af3394d79d156087fa4d3ffece9c319e832e5419
james7132 <james.siqi.liu@gmail.com>
2017-07-10 21:55:35 -0700

    Make the movement lerp actually transition

--- Assets/Dependencies/SmashBrew/Character/Components/MovementState.cs
+++ Assets/Dependencies/SmashBrew/Character/Components/MovementState.cs
@@ -235,7 +235,7 @@ namespace HouraiTeahouse.SmashBrew.Characters {
                 LedgeMovement();
             } else {
                 var movementInput = InputState.Movement;
-                movement.Speed.x = movementInput.x * CurrentState.Data.MovementSpeed.Max;
+                movement.Speed.x = Mathf.Sign(movementInput.x) * CurrentState.Data.MovementSpeed.Lerp(Mathf.Abs(movementInput.x));
                 if (IsGrounded) {
                     IsFastFalling = false;
                     if (JumpCount != MaxJumpCount)
