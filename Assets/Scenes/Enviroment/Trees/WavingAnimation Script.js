9e00f68a533341afd6ba53452f50d63b66e555f3
Gabo7 <gabriel-sierra@hotmail.com>
2017-07-12 18:19:43 -0500

    Forest of Magic fixes
    -Lightning updated to current unity lightning models
    -Added script so branches wave with the wind

---
+++ Assets/Scenes/Enviroment/Trees/WavingAnimation Script.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	var waving : Vector4 = GetComponent.<Renderer>().sharedMaterial.GetVector("_WaveAndDistance");
+	var wavingPower : float = waving.w;
+	
+	waving.x += (Time.deltaTime*0.1*wavingPower*Random.value);
+	waving.y -= (Time.deltaTime*0.1*wavingPower*Random.value* Random.value);
+	
+	GetComponent.<Renderer>().sharedMaterial.SetVector("_WaveAndDistance", Vector4(waving.x,waving.y,waving.z,waving.w));
+}
