f7d2414d837db16ac08320fd683ba3e2bf3ba6bb
james7132 <james.siqi.liu@gmail.com>
2017-07-17 21:02:30 -0700

    Fix player callbacks not being unregistered

--- Assets/Dependencies/SmashBrew/UI/Match/PlayerInfoGUI.cs
+++ Assets/Dependencies/SmashBrew/UI/Match/PlayerInfoGUI.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -19,6 +20,8 @@ namespace HouraiTeahouse.SmashBrew.UI {
         [SerializeField]
         RectTransform _spacePrefab;
 
+        Action[] _callbacks;
+
         /// <summary> Unity callback. Called on object instantiation. </summary>
         void Awake() {
             if (!_container) {
@@ -37,15 +40,36 @@ namespace HouraiTeahouse.SmashBrew.UI {
             initialSpace.name = _spacePrefab.name;
             _finalSpace.name = _spacePrefab.name;
 
-            foreach (var player in PlayerManager.Instance.MatchPlayers) {
+            var players = PlayerManager.Instance.MatchPlayers;
+
+            _callbacks = new Action[players.Count];
+
+            var i = 0;
+            foreach (var player in players) {
                 RectTransform display = Instantiate(_prefab);
                 display.transform.SetParent(_container.transform, false);
                 LayoutRebuilder.MarkLayoutForRebuild(display);
                 display.GetComponentsInChildren<IDataComponent<Player>>().SetData(player);
                 display.name = "Player {0} Display".With(player.ID + 1);
                 display.gameObject.SetActive(player.Type.IsActive);
-                player.Changed += () => display.gameObject.SetActive(player.Type.IsActive);
+                _callbacks[i] = () => display.gameObject.SetActive(player.Type.IsActive);
+                player.Changed += _callbacks[i];
                 _finalSpace.transform.SetAsLastSibling();
+                i++;
+            }
+        }
+
+        /// <summary>
+        /// This function is called when the MonoBehaviour will be destroyed.
+        /// </summary>
+        void OnDestroy() {
+            if (_callbacks == null)
+                return;
+            var players = PlayerManager.Instance.MatchPlayers;
+            var i = 0;
+            foreach (var player in players) {
+                player.Changed -= _callbacks[i];
+                i++;
             }
         }
 
