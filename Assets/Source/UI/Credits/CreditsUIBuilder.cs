0c92f0227819abd8a8e34f2284df83a02dbe6228
james7132 <james.siqi.liu@gmail.com>
2017-07-22 18:41:24 -0700

    Better looking credits menu

--- Assets/Source/UI/Credits/CreditsUIBuilder.cs
+++ Assets/Source/UI/Credits/CreditsUIBuilder.cs
@@ -19,7 +19,10 @@ namespace HouraiTeahouse.FantasyCrescendo {
         float _indent = 0.05f;
 
         [SerializeField]
-        float _optionHeight = 40;
+        float _creditHeight = 40;
+
+        [SerializeField]
+        float _spaceHeight = 20;
 
         [Header("Subcomponents")]
         [SerializeField]
@@ -30,10 +33,12 @@ namespace HouraiTeahouse.FantasyCrescendo {
         RectTransform optionLabelTemplate;
 
         [SerializeField]
+        int _maxContributorsPerLine = 3;
+
+        [SerializeField]
         CreditsAsset _credits;
 
         Dictionary<Type, RectTransform> _prefabs;
-
 
         /// <summary>
         /// Start is called on the frame when a script is enabled just before
@@ -48,21 +53,70 @@ namespace HouraiTeahouse.FantasyCrescendo {
             if (viewContainer == null)
                 viewContainer = transform;
             foreach (CreditsAsset.Category category in _credits.Categories) {
-                var categoryLabel = Instantiate(categoryLabelTemplate);
-                SetNameAndText(categoryLabel.gameObject, category.Name);
-                BuildLayout(categoryLabel.gameObject);
-                categoryLabel.SetParent(viewContainer, false);
-                categoryLabel.gameObject.SetUIText(category.Name);
-                foreach (string contributor in category.Contributors) {
-                    var container = new GameObject(contributor, typeof(RectTransform));
-                    BuildLayout(container);
-                    container.transform.SetParent(viewContainer, false);
+                if (category.Contributors.Length <= 0)
+                    continue;
+                if (category.Contributors.Length == 1)
+                    BuildSingleView(category, viewContainer);
+                if (category.Contributors.Length > 1) 
+                    BuildFullView(category, viewContainer);
+                var space = new GameObject("Space", typeof(RectTransform));
+                BuildLayout(space, _spaceHeight);
+                space.transform.SetParent(viewContainer, false);
+            }
+        }
+
+        void BuildSingleView(CreditsAsset.Category category, Transform viewContainer) {
+            var container = new GameObject(category.Name, typeof(RectTransform));
+            BuildLayout(container, _creditHeight);
+            var categoryLabel = Instantiate(categoryLabelTemplate);
+            categoryLabel.SetParent(container.transform, false);
+            FillRect(categoryLabel, _indent, _labelSize);
+            SetNameAndText(categoryLabel.gameObject, category.Name);
+            foreach (var text in categoryLabel.GetComponentsInChildren<Text>())
+                text.alignment = TextAnchor.MiddleLeft;
+            container.transform.SetParent(viewContainer, false);
+            var optionLabel = Instantiate(optionLabelTemplate);
+            optionLabel.SetParent(container.transform, false);
+            SetNameAndText(optionLabel.gameObject, category.Contributors[0]);
+            FillRect(optionLabel, _labelSize, (1 - (_indent + _labelSize)));
+            foreach (var text in optionLabel.GetComponentsInChildren<Text>())
+                text.alignment = TextAnchor.MiddleRight;
+        }
+
+        void BuildFullView(CreditsAsset.Category category, Transform viewContainer) {
+            var categoryLabel = Instantiate(categoryLabelTemplate);
+            SetNameAndText(categoryLabel.gameObject, category.Name);
+            BuildLayout(categoryLabel.gameObject, _creditHeight);
+            categoryLabel.SetParent(viewContainer, false);
+            categoryLabel.gameObject.SetUIText(category.Name);
+            int segmentCount = 1;
+            foreach (var segment in Segment(category.Contributors.OrderBy(s => s), _maxContributorsPerLine)) {
+                var count = segment.Length;
+                var size = 1 / (float) count;
+                var container = new GameObject(category.Name + " " + segmentCount, typeof(RectTransform));
+                BuildLayout(container, _creditHeight);
+                container.transform.SetParent(viewContainer, false);
+                for (var i = 0; i < segment.Length; i++) {
                     var optionLabel = Instantiate(optionLabelTemplate);
                     optionLabel.SetParent(container.transform, false);
-                    SetNameAndText(container, contributor);
-                    FillRect(optionLabel, _indent, _labelSize);
+                    SetNameAndText(optionLabel.gameObject, segment[i]);
+                    FillRect(optionLabel, i * size, size);
+                }
+                segmentCount++;
+            }
+        }
+
+        IEnumerable<T[]> Segment<T>(IEnumerable<T> input, int size) {
+            var segment = new List<T>();
+            foreach (var value in input) {
+                segment.Add(value);
+                if (segment.Count >= size) {
+                    yield return segment.ToArray();
+                    segment.Clear();
                 }
             }
+            if (segment.Count > 0)
+                yield return segment.ToArray();
         }
 
         void SetNameAndText(GameObject label, string text) {
@@ -70,9 +124,9 @@ namespace HouraiTeahouse.FantasyCrescendo {
             label.gameObject.SetUIText(text);
         } 
 
-        void BuildLayout(GameObject container) {
+        void BuildLayout(GameObject container, float height) {
             var layout = container.GetOrAddComponent<LayoutElement>();
-            layout.preferredHeight = _optionHeight;
+            layout.preferredHeight = height;
             layout.flexibleHeight = 0f;
         }
 
