edd7155a8a486fbefa664975658d2d19fcd21b97
james7132 <james.siqi.liu@gmail.com>
2017-07-22 17:16:26 -0700

    Create Credits Asset

---
+++ Assets/Source/CreditsAsset.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HouraiTeahouse {
+
+    [CreateAssetMenu(menuName = "Fantasy Crescendo/Credits Asset")]
+    public class CreditsAsset : ScriptableObject {
+
+        [Serializable]
+        public class Category {
+            public string Name;
+            public string[] Contributors;
+
+            public override string ToString() {
+                if (Contributors != null && Contributors.Length == 1)
+                    return Name + ": " + Contributors[0];
+                return Name;
+            }
+        }
+
+        [SerializeField]
+        Category[] _categories;
+
+    }
+
+
+}
