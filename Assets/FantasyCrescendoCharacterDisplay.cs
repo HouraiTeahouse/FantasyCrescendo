1361f32e89fd11989d26fa44e23880c5c4f55da1
james7132 <james.siqi.liu@gmail.com>
2017-07-30 22:37:50 -0700

    Better emulate the mockup Nana created for the main menu

---
+++ Assets/FantasyCrescendoCharacterDisplay.cs
@@ -0,0 +1,40 @@
+﻿using HouraiTeahouse.SmashBrew;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HouraiTeahouse.FantasyCrescendo {
+
+    public class FantasyCrescendoCharacterDisplay : MonoBehaviour {
+
+        [SerializeField]
+        Image _image;
+
+        /// <summary>
+        /// Awake is called when the script instance is being loaded.
+        /// </summary>
+        void Start() {
+            var characters = DataManager.Characters;
+            if (characters == null)
+                return;
+            var test = characters.Where(c => c.PalleteCount >= 1)
+                .ToArray()
+                .Random();
+            Log.Debug(test);
+            test.GetPortrait(0)
+                .LoadAsync()
+                .Then(sprite => {
+                    if (_image == null)
+                        return;
+                    if (sprite == null)
+                        _image.enabled = false;
+                    else
+                        _image.sprite = sprite;
+                });
+        }
+
+    }
+
+}
