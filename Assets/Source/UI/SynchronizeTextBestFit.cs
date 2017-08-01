77302d6d0bd7cfec70dd096f45162c06f8b0b221
james7132 <james.siqi.liu@gmail.com>
2017-07-31 23:52:57 -0700

    Fix #236

--- Assets/Source/UI/SynchronizeTextBestFit.cs
+++ Assets/Source/UI/SynchronizeTextBestFit.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 namespace HouraiTeahouse.SmashBrew {
 
-    public class SynchronizeTextBestFit : MonoBehaviour {
+    public class SynchronizeTextBestFit : UIBehaviour {
 
         [SerializeField]
         Text[] _texts;
@@ -14,7 +15,18 @@ namespace HouraiTeahouse.SmashBrew {
         /// <summary>
         /// This function is called when the object becomes enabled and active.
         /// </summary>
-        void OnEnable() {
+        protected override void OnEnable() {
+            base.OnEnable();
+            StartCoroutine(UpdateSizes());
+        }
+
+        protected override void OnRectTransformDimensionsChange() {
+            foreach (var text in _texts)
+                if (text != null)
+                    text.resizeTextMaxSize = 10000;
+            if (!isActiveAndEnabled)
+                return;
+            StopAllCoroutines();
             StartCoroutine(UpdateSizes());
         }
 
@@ -23,7 +35,6 @@ namespace HouraiTeahouse.SmashBrew {
             yield return null;
             while (true) {
                 var minSize = _texts.Min(t => t.cachedTextGenerator.fontSizeUsedForBestFit);
-                Log.Debug(minSize);
                 foreach (var text in _texts)
                     text.resizeTextMaxSize = minSize;
                 yield return null;
