657fcc20f9d88c724c8f4c769df4d41c2592c659
James Liu <contact@jamessliu.com>
2018-01-02 16:25:45 -0800

    Move Config into HouraiConfig

--- Assets/Code/src/Data/DataManager.cs
+++ Assets/Code/src/Data/DataManager.cs
@@ -5,14 +5,8 @@ namespace HouraiTeahouse.FantasyCrescendo {
 public class DataManager : MonoBehaviour {
 
   public CharacterData[] Characters;
-  public ScriptableObject[] Configs;
 
   void Awake() {
-    foreach (var config in Configs) {
-      Config.Register(config);
-      Debug.LogFormat("Loaded configuration: {0}", config.GetType().Name);
-    }
-
     var registry = Registry.Get<CharacterData>();
     foreach (var character in Characters) {
       registry.Add(character);
