f6c0a0b05056be94efc92080ebcdf6f3ef624b14
James Liu <contact@jamessliu.com>
2018-01-07 22:05:20 -0800

    Add Credits

---
+++ Assets/Code/src/CreditsAsset.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HouraiTeahouse {
+
+/// <summary>
+/// Custom asset for allowing easy editing of the game's credits.!
+/// Used with <see cref="CreditsUIBuilder"/> to automatically generate the UI on the credits screen.
+/// </summary>
+[CreateAssetMenu(menuName = "Fantasy Crescendo/Credits Asset")]
+public class CreditsAsset : ScriptableObject {
+
+  [Serializable]
+  public class Category {
+    //TODO(james7132): Make these readonly.
+    public string Name;
+    public string[] Contributors;
+
+    public override string ToString() {
+      if (Contributors?.Length == 1) {
+        return $"{Name}: {Contributors[0]}";
+      }
+      return Name;
+    }
+  }
+
+  public Category[] Categories;
+
+}
+
+}
