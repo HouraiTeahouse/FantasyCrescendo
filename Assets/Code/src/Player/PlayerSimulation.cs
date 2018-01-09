7fddb51a588aff9a70cc5b57c9910ab8aa5077cf
James Liu <contact@jamessliu.com>
2018-01-08 20:57:47 -0800

    Fix NullReferenceExceptions when not initialized

--- Assets/Code/src/Player/PlayerSimulation.cs
+++ Assets/Code/src/Player/PlayerSimulation.cs
@@ -1,6 +1,7 @@
 using HouraiTeahouse.Tasks;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace HouraiTeahouse.FantasyCrescendo {
 
@@ -13,21 +14,21 @@ public class PlayerSimulation : IInitializable<PlayerConfig>, ISimulation<Player
   public ITask Initialize(PlayerConfig config) {
     var selection = config.Selection;
     var character = Registry.Get<CharacterData>().Get(selection.CharacterID);
-    Model = Object.Instantiate(character.Prefab);
-    Model.name = string.Format("Player {0} Simulation ({1}, {2})",
-                               config.PlayerID + 1, character.name,
-                               selection.Pallete);
+    return character.Prefab.LoadAsync().Then(prefab => {
+      Assert.IsNotNull(prefab);
+      Model = Object.Instantiate(prefab);
+      Model.name = $"Player {config.PlayerID + 1} Simulation ({character.name}, {selection.Pallete})";
 
-    PlayerUtil.DestroyAll(Model, typeof(Renderer), typeof(MeshFilter));
+      PlayerUtil.DestroyAll(Model, typeof(Renderer), typeof(MeshFilter));
 
-    var task = Model.Broadcast<ICharacterComponent>(
-        component => component.Initialize(config, false));
+      var task = Model.Broadcast<ICharacterComponent>(
+          component => component.Initialize(config, false));
 
-    SimulationComponents = Model.GetComponentsInChildren<ISimulation<PlayerState, PlayerInputContext>>();
-    PresimulateComponents = SimulationComponents.OfType<ICharacterSimulation>()
-                                                .ToArray();
-
-    return task;
+      SimulationComponents = Model.GetComponentsInChildren<ISimulation<PlayerState, PlayerInputContext>>();
+      PresimulateComponents = SimulationComponents.OfType<ICharacterSimulation>()
+                                                  .ToArray();
+      return task;
+    });
   }
 
   public void Presimulate(PlayerState state) {
