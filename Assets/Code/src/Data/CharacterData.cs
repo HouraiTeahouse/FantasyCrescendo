f67d2bfe6290af6e6e3a1ddbdd4657842cc691fe
James Liu <contact@jamessliu.com>
2018-01-08 20:50:16 -0800

    Change CharacterData to use loadables

--- Assets/Code/src/Data/CharacterData.cs
+++ Assets/Code/src/Data/CharacterData.cs
@@ -1,3 +1,4 @@
+using HouraiTeahouse.Loadables;
 using System;
 using UnityEngine;
 using Random = System.Random;
@@ -13,9 +14,19 @@ public class CharacterData : ScriptableObject, IIdentifiable {
   [SerializeField] uint _id;
   public string ShortName;
   public string LongName;
-  public GameObject Prefab;
-  public Sprite Icon;
-  public Sprite[] Portraits;
+  public bool IsSelectable = true;
+  public bool IsVisible = true;
+
+  [SerializeField, Resource(typeof(GameObject))] string _prefab;
+  [SerializeField, Resource(typeof(Sprite))] string _icon;
+  [SerializeField, Resource(typeof(Sprite))] string[] _portraits;
+
+  public IAsset<GameObject> Prefab => Asset.Get<GameObject>(_prefab);
+  public IAsset<Sprite> Icon => Asset.Get<Sprite>(_icon);
+
+  public IAsset<Sprite> GetPortrait(int index) {
+    return Asset.Get<Sprite>(_portraits[index % _portraits.Length]);
+  }
 
   public uint Id {
     get { return _id; }
