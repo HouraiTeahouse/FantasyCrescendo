8e5e261a6e3ba53c286fdef3004c6f5a574a49ad
James Liu <contact@jamessliu.com>
2018-01-10 21:02:45 -0800

    Add SceneData

---
+++ Assets/Code/src/Runtime/Data/SceneData.cs
@@ -0,0 +1,24 @@
+﻿using HouraiTeahouse.Loadables;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HouraiTeahouse.FantasyCrescendo {
+
+[CreateAssetMenu(menuName = "Fantasy Crescendo/Scene (Stage)")]
+public class SceneData : ScriptableObject {
+  public string Name;
+
+  public bool IsStage = true;
+  public bool IsSelectable = true;
+  public bool IsVisible = true;
+  public bool IsDebug;
+
+  [SerializeField, Scene] string _scene;
+
+  public IScene GameScene => Scene.Get(_scene);
+
+  public BGM[] Music;
+}
+
+}
