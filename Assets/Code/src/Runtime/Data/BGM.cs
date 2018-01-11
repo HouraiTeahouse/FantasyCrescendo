33f357ab20165029cac724dbdbe05c348d88389a
james7132 <james.siqi.liu@gmail.com>
2018-01-10 21:02:45 -0800

    Add SceneData

---
+++ Assets/Code/src/Runtime/Data/BGM.cs
@@ -0,0 +1,18 @@
+﻿using HouraiTeahouse.Loadables;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HouraiTeahouse.FantasyCrescendo {
+
+[CreateAssetMenu(menuName = "Fantasy Crescendo/BGM")]
+public class BGM : ScriptableObject {
+  [SerializeField, Resource(typeof(AudioClip))]
+  string _audioClip;
+  public IAsset<AudioClip> Clip => Asset.Get<AudioClip>(_audioClip);
+  public string Author;
+  public string Original;
+  [Range(0f, 1f)] public float DefaultWeight;
+}
+
+}
