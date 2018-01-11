5a687ff61532db413957875e508387df88bb13bf
james7132 <james.siqi.liu@gmail.com>
2018-01-11 01:37:25 -0800

    Add Stage Bootstrap

---
+++ Assets/Code/src/Runtime/Components/SceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using HouraiTeahouse.Loadables;
+using HouraiTeahouse.Tasks;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Scene = HouraiTeahouse.Loadables.Scene;
+
+namespace HouraiTeahouse.FantasyCrescendo {
+
+public class SceneLoader : MonoBehaviour {
+
+  public LoadSceneMode Mode;
+  [SerializeField, Scene] string[] _scenes;
+
+  /// <summary>
+  /// Awake is called when the script instance is being loaded.
+  /// </summary>
+  void Awake() {
+    Task.All(_scenes.Select(Scene.Get).Select(s => s.LoadAsync(Mode)))
+      .Then(() => Debug.Log("Scenes loaded!"));
+  }
+
+}
+
+}
