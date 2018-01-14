c781eab41c3174331324418f5b83e09aedbb2cf8
James Liu <contact@jamessliu.com>
2018-01-14 03:42:45 -0800

    Add CameraController/CameraTargets

---
+++ Assets/Code/src/Runtime/Character/Components/CharacterCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace HouraiTeahouse.FantasyCrescendo {
+
+public class CharacterCamera : MonoBehaviour, IPlayerComponent {
+
+  public Task Initialize(PlayerConfig config, bool isView = false) {
+    if (isView) {
+      var matchCameraTarget = FindObjectOfType<MatchCameraTarget>();
+      if (matchCameraTarget != null) {
+        matchCameraTarget.RegisterTarget(transform);
+      }
+    }
+    return Task.CompletedTask;
+  }
+
+  /// <summary>
+  /// This function is called when the MonoBehaviour will be destroyed.
+  /// </summary>
+  void OnDestroy() {
+    var matchCameraTarget = FindObjectOfType<MatchCameraTarget>();
+    if (matchCameraTarget != null) {
+      matchCameraTarget.UnregisterTarget(transform);
+    }
+  }
+
+}
+
+}
---
+++ Assets/Code/src/Runtime/Stage/Camera/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace HouraiTeahouse.FantasyCrescendo {
+
+public class CameraController : MonoBehaviour {
+
+  public static CameraController Instance { get; set; }
+  public CameraTarget Target;
+  public Camera Camera;
+
+  /// <summary>
+  /// Awake is called when the script instance is being loaded.
+  /// </summary>
+  void Awake() {
+    if (Camera == null) {
+      Camera = GetComponentInChildren<Camera>();
+    }
+  }
+
+  /// <summary>
+  /// LateUpdate is called every frame, if the Behaviour is enabled.
+  /// It is called after all Update functions have been called.
+  /// </summary>
+  void LateUpdate() {
+    if (Target == null) return;
+    var targetTransform = Target.transform;
+    transform.position = targetTransform.position;
+    transform.rotation = targetTransform.rotation;
+    if (Camera != null) {
+      Camera.fieldOfView = Target.FieldOfView;
+    }
+  }
+
+}
+
+}
