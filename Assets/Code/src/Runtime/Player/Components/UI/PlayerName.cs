7eb55c57a006a80cbe91170d699bf13020e87524
James Liu <contact@jamessliu.com>
2018-01-14 06:57:54 -0800

    Add Player Indicators

---
+++ Assets/Code/src/Runtime/Player/Components/UI/PlayerName.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HouraiTeahouse.FantasyCrescendo {
+
+public class PlayerName : MonoBehaviour, IInitializable<PlayerConfig> {
+
+  public Text Text;
+  public string Format;
+
+  public Task Initialize(PlayerConfig config) {
+    if (Text != null) {
+      Text.text = string.Format(Format, config.PlayerID);
+    }
+    return Task.CompletedTask;
+  }
+
+}
+    
+}
+
