6abc05f0067082a5ec43a53a07df0856c89b5992
James Liu <contact@jamessliu.com>
2018-01-14 01:22:26 -0800

    Add unit tests for Character and SceneData

--- Assets/Code/src/Runtime/Data/CharacterData.cs
+++ Assets/Code/src/Runtime/Data/CharacterData.cs
@@ -1,5 +1,7 @@
 using HouraiTeahouse.Loadables;
 using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 using UnityEngine;
 using Random = System.Random;
 
@@ -11,19 +13,39 @@ namespace HouraiTeahouse.FantasyCrescendo {
 [CreateAssetMenu(menuName = "Fantasy Crescendo/Character")]
 public class CharacterData : GameDataBase {
 
+  ReadOnlyCollection<IAsset<Sprite>> _portraitsAssets;
+
   public string ShortName;
   public string LongName;
 
   [SerializeField, Resource(typeof(GameObject))] string _prefab;
   [SerializeField, Resource(typeof(Sprite))] string _icon;
   [SerializeField, Resource(typeof(Sprite))] string[] _portraits;
+  [SerializeField, Resource(typeof(SceneData))] string _homeStage;
+  [SerializeField, Resource(typeof(AudioClip))] string _victoryTheme;
 
   public IAsset<GameObject> Prefab => Asset.Get<GameObject>(_prefab);
   public IAsset<Sprite> Icon => Asset.Get<Sprite>(_icon);
+  public IAsset<SceneData> HomeStage => Asset.Get<SceneData>(_homeStage);
+  public IAsset<AudioClip> VictoryTheme => Asset.Get<AudioClip>(_victoryTheme);
 
-  public IAsset<Sprite> GetPortrait(int index) {
-    return Asset.Get<Sprite>(_portraits[index % _portraits.Length]);
+  public ReadOnlyCollection<IAsset<Sprite>> Portraits {
+    get {
+      if (_portraitsAssets == null) {
+        var portraitAssets = _portraits.Select(path => Asset.Get<Sprite>(path)).ToArray();
+        _portraitsAssets = new ReadOnlyCollection<IAsset<Sprite>>(portraitAssets);
+      }
+      return _portraitsAssets;
+    }
   }
+
+  public void Unload() {
+    new ILoadable[] { Prefab, Icon, HomeStage, VictoryTheme }
+      .Concat(Portraits)
+      .UnloadAll();
+  }
+
+  public override string ToString() => $"Character ({name})";
 
 }
 
