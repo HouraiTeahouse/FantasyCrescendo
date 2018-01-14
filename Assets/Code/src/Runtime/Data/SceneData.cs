4edb9c3f2f907feb8a13d377ad68ab07a4ef1b4c
james7132 <james.siqi.liu@gmail.com>
2018-01-14 01:22:26 -0800

    Add unit tests for Character and SceneData

--- Assets/Code/src/Runtime/Data/SceneData.cs
+++ Assets/Code/src/Runtime/Data/SceneData.cs
@@ -7,14 +7,19 @@ namespace HouraiTeahouse.FantasyCrescendo {
 public class SceneData : GameDataBase {
 
   public bool IsStage = true;
-
   public string Name;
 
   [SerializeField, Scene] string _scene;
-
-  public IScene GameScene => Scene.Get(_scene);
+  [SerializeField, Resource(typeof(Sprite))] string _icon;
+  [SerializeField, Resource(typeof(Sprite))] string _previewImage;
 
   public BGM[] Music;
+
+  public IScene GameScene => Scene.Get(_scene);
+  public IAsset<Sprite> Icon => Asset.Get<Sprite>(_icon);
+  public IAsset<Sprite> PreviewImage => Asset.Get<Sprite>(_previewImage);
+
+  public override string ToString() => $"Scene ({name})";
 }
 
 }
---
+++ Assets/Code/test/Editor/Match/Data/AbstractDataTest.cs
@@ -0,0 +1,25 @@
+﻿using HouraiTeahouse;
+using HouraiTeahouse.FantasyCrescendo;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+internal class AbstractDataTest<T> where T : GameDataBase {
+
+    protected delegate object AssetFunc(T data);
+
+    protected delegate IEnumerable AssetManyFunc(T data);
+
+    protected static IEnumerable<T> data;
+
+    public static IEnumerable<object[]> TestData() {
+        if (data == null) {
+          data = EditorAssetUtil.LoadAll<T>().Where(d => d != null && d.IsSelectable && d.IsVisible);
+        }
+        foreach (var datum in data) {
+          yield return new object[] {datum};
+        }
+    }
+
+}
