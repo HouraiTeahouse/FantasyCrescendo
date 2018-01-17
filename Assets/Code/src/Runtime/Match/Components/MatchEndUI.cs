505c6a6525aa91ab4e91a3dea1bdb2931d66b112
james7132 <james.siqi.liu@gmail.com>
2018-01-16 22:01:15 -0800

    Add Match End UI display

---
+++ Assets/Code/src/Runtime/Match/Components/MatchEndUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+namespace HouraiTeahouse.FantasyCrescendo {
+
+public class MatchEndUI : MonoBehaviour {
+
+  public Object GameEndedDisplay;
+  public Object TimeUpDisplay;
+  public float PostGameWait = 5f;
+
+  /// <summary>
+  /// Awake is called when the script instance is being loaded.
+  /// </summary>
+  void Awake() {
+    Mediator.Global.CreateUnityContext(this).Subscribe<MatchEndEvent>(OnMatchEnd);
+  }
+
+  async Task OnMatchEnd(MatchEndEvent evt) {
+    if (evt.MatchConfig.Time > 0 && evt.MatchState.Time <= 0) {
+      ObjectUtil.SetActive(TimeUpDisplay, true);
+    } else {
+      ObjectUtil.SetActive(GameEndedDisplay, true);
+    }
+    await Task.Delay((int)(PostGameWait * 1000));
+  }
+
+}
+
+}
