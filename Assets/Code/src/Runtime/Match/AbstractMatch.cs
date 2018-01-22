5e3d5687d06ee6fd84439897fb87df5bc993d470
James Liu <contact@jamessliu.com>
2018-01-21 23:08:20 -0800

    Implement Stage Editable Spawn Points

--- Assets/Code/src/Runtime/Match/AbstractMatch.cs
+++ Assets/Code/src/Runtime/Match/AbstractMatch.cs
@@ -1,8 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.SceneManagement;
+using Object = UnityEngine.Object;
 
 namespace HouraiTeahouse.FantasyCrescendo {
 
@@ -66,6 +68,29 @@ public class DefaultMatch : Match {
   }
 
   MatchState CreateInitialState(MatchConfig config) {
+    var tag = Config.Get<SceneConfig>().SpawnTag;
+    var startPositions = GameObject.FindGameObjectsWithTag(tag);
+    Debug.LogError(startPositions.Length);
+    if (startPositions.Length > 0) {
+      return CreateInitialStateByTransform(config, startPositions);
+    } 
+    return CreateInitialStateSimple(config);
+  }
+
+  MatchState CreateInitialStateByTransform(MatchConfig config, GameObject[] startPositions) {
+    var initialState = new MatchState(config);
+    startPositions = startPositions.OrderBy(s => s.transform.GetSiblingIndex()).ToArray();
+    for (uint i = 0; i < initialState.PlayerCount; i++) {
+      var startPos = startPositions[i % startPositions.Length].transform;
+      var state = initialState.GetPlayerState(i);
+      state.Position = startPos.position;
+      state.Direction = startPos.transform.forward.x >= 0;
+      initialState.SetPlayerState(i, state);
+    }
+    return initialState;
+  }
+
+  MatchState CreateInitialStateSimple(MatchConfig config) {
     var initialState = new MatchState(config);
     for (uint i = 0; i < initialState.PlayerCount; i++) {
       var state = initialState.GetPlayerState(i);
@@ -74,6 +99,7 @@ public class DefaultMatch : Match {
     }
     return initialState;
   }
+
 }
 
 }
