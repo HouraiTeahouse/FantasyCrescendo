07c4f8ae53c1b1f8fef1da63979f228b5ef0858f
James Liu <contact@jamessliu.com>
2018-02-02 21:13:14 -0800

    Add component tests for characters

--- Assets/Code/test/Editor/Match/Data/CharacterDataTest.cs
+++ Assets/Code/test/Editor/Match/Data/CharacterDataTest.cs
@@ -1,10 +1,13 @@
-﻿using HouraiTeahouse.FantasyCrescendo;
+﻿using HouraiTeahouse;
+using HouraiTeahouse.FantasyCrescendo;
 using HouraiTeahouse.FantasyCrescendo.Characters;
+using HouraiTeahouse.FantasyCrescendo.Players;
+using NUnit.Framework;
+using System;  
+using System.Collections;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.TestTools;
-using NUnit.Framework;
-using System.Collections;
 
 /// <summary> 
 /// Tests for CharacterData instances.
@@ -16,19 +19,42 @@ using System.Collections;
 [Parallelizable]
 internal class CharacterDataTest : AbstractDataTest<CharacterData> {
 
-  [Test, TestCaseSource("TestData")]
+  public static IEnumerable ComponentCases() => Permutation.Generate(AllData, RequiredTypes);
+
+  static Type[] RequiredTypes => new Type[] {
+    typeof(CharacterAnimation),
+    typeof(CharacterPhysics),
+    typeof(CharacterLedge),
+    typeof(CharacterIndicator),
+    typeof(CharacterCamera),
+    typeof(CharacterRespawn),
+    typeof(CharacterStateMachine),
+    typeof(CharacterColor),
+    typeof(CharacterShield),
+    typeof(PlayerActive),
+    typeof(CharacterHitboxController),
+    typeof(CharacterMovement),
+  };
+
+  [Test, TestCaseSource("AllData")]
   public void has_a_prefab(CharacterData character) {
     Assert.NotNull(character.Prefab.Load());
   }
 
-  [Test, TestCaseSource("TestData")]
+  [Test, TestCaseSource("AllData")]
   public void has_equal_pallete_and_portrait_counts(CharacterData character) {
     var swap = character.Prefab.Load().GetComponent<CharacterColor>();
     Assert.NotNull(swap);
     Assert.AreEqual(swap.Count, character.Portraits.Count);
   }
 
-  [Test, TestCaseSource("TestData")]
+  [TestCaseSource("ComponentCases")]
+  public void has_component(CharacterData character, Type type) {
+    var prefab = character.Prefab.Load();
+    Assert.IsNotNull(prefab.GetComponentInChildren(type));
+  }
+
+  [Test, TestCaseSource("AllData")]
   public void has_valid_portraits(CharacterData character) {
     var portraits = character.Portraits;
     foreach (var portrait in character.Portraits) {
@@ -36,19 +62,20 @@ internal class CharacterDataTest : AbstractDataTest<CharacterData> {
     }
   }
 
-  [Test, TestCaseSource("TestData")]
+  [Test, TestCaseSource("AllData")]
   public void has_valid_icons(CharacterData character) {
     Assert.NotNull(character.Icon.Load());
   }
 
-  [Test, TestCaseSource("TestData")]
+  [Test, TestCaseSource("AllData")]
   public void has_valid_home_stage(CharacterData character) {
     Assert.NotNull(character.HomeStage);
   }
 
-  [Test, TestCaseSource("TestData")]
+  [Test, TestCaseSource("AllData")]
   public void has_valid_victory_theme(CharacterData character) {
     Assert.NotNull(character.VictoryTheme.Load());
   }
+
 
 }
