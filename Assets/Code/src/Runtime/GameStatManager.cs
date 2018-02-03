859119c182ee18465a280e5af5eabf4b6e018f2f
james7132 <james.siqi.liu@gmail.com>
2018-02-03 05:56:07 -0800

    Add Game Stat Tracking and remove debug logs

---
+++ Assets/Code/src/Runtime/GameStatManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class StatTracker {
+  public double Value { get; protected set; }
+  public virtual void Update(double value) => Value = value;
+  public void Reset() => Value = 0f;
+}
+
+public class CounterStatTracker : StatTracker {
+  public override void Update(double value) => Value += value;
+}
+
+public class RollingAverageStatTracker : StatTracker {
+  public const double kRetentionRate = 0.1f;
+  public override void Update(double value) => Value += (value - Value) * kRetentionRate;
+}
+
+public static class GameStats {
+
+  public static readonly NetworkingStats Network = new NetworkingStats();
+
+  public class NetworkingStats {
+    public readonly ClientStats Client = new ClientStats();
+  }
+
+  public class ClientStats {
+    public readonly StatTracker UnconfirmedInputs = new RollingAverageStatTracker();
+  }
+
+}
