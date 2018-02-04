eaf09f659bd74c640398e579ad4e9a91c9efff9c
James Liu <contact@jamessliu.com>
2018-02-04 04:16:53 -0800

    Add Version Text to Debug Builds

---
+++ Assets/Code/src/Runtime/UI/VersionText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HouraiTeahouse.FantasyCrescendo {
+
+public class VersionText : MonoBehaviour {
+
+  public Text Text;
+  public string Format;
+
+  /// <summary>
+  /// Awake is called when the script instance is being loaded.
+  /// </summary>
+  void Awake() {
+    if (Debug.isDebugBuild) {
+      if (string.IsNullOrEmpty(Format)) {
+        Text.text = Application.version;
+      } else {
+        Text.text = string.Format(Format, Application.version);
+      }
+      Destroy(this);
+    } else {
+      Destroy(gameObject);
+    }
+  }
+
+}
+
+}
