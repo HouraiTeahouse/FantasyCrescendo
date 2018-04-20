57638e700b3d658851e8285653974b805e0ba8c0
James Liu <contact@jamessliu.com>
2018-04-20 00:02:37 -0700

    Basic main menu design by Bakaretsu

---
+++ Assets/Data/Scenes/Menus/MainMenu/ScrollingBackgroundImage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HouraiTeahouse.FantasyCrescendo.UI {
+
+public class ScrollingBackgroundImage : MonoBehaviour {
+
+  public RawImage image;
+  public Vector2 ScrollSpeed;
+
+	// Update is called once per frame
+	void Update () {
+    if (image == null) return;
+    var rect = image.uvRect;
+    rect.position += ScrollSpeed * Time.deltaTime;
+    image.uvRect = rect;
+	}
+
+}
+
+}
