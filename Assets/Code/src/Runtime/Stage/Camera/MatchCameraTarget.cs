c2e3d8d1d2003cb49b698736bcd312745bcfdca1
James Liu <contact@jamessliu.com>
2018-07-18 01:29:22 -0700

    Better camera

--- Assets/Code/src/Runtime/Stage/Camera/MatchCameraTarget.cs
+++ Assets/Code/src/Runtime/Stage/Camera/MatchCameraTarget.cs
@@ -7,11 +7,14 @@ namespace HouraiTeahouse.FantasyCrescendo {
 public class MatchCameraTarget : MonoBehaviour {
 
   public CameraTarget CameraTarget;
-  public float CameraSpeed = 1f;
+  public float SmoothTime = 0.5f;
   public Vector2 FoVRange;
-  public Vector2 Padding;
+  public float WidthPadding = 5f;
   public Vector3 TargetPositionBias;
+  public float MaxSpreadDistance = 50f;
   public List<Transform> Targets;
+
+  private Vector3 velocity;
 
   /// <summary>
   /// Awake is called when the script instance is being loaded.
@@ -24,50 +27,36 @@ public class MatchCameraTarget : MonoBehaviour {
   }
 
   /// <summary>
-  /// Update is called every frame, if the MonoBehaviour is enabled.
+  /// LateUpdate is called every frame, if the Behaviour is enabled.
+  /// It is called after all Update functions have been called.
   /// </summary>
-  void Update() {
-    var count = 0;
-    float dt = Time.deltaTime;
+  void LateUpdate() {
+    var bounds = GetTargetBounds();
+    if (bounds == null) return;
+    var center = bounds.Value.center;
+    var width = bounds.Value.size.x + WidthPadding;
 
-    //Find the Bounds in which
-    Vector3 sum = Vector3.zero;
-    Vector3 min = Vector3.one * float.PositiveInfinity;
-    Vector3 max = Vector3.one * float.NegativeInfinity;
-    foreach (Transform target in Targets) {
-      if (target == null || !target.gameObject.activeInHierarchy) {
-        continue;
+    center.z = transform.position.z;
+    transform.position = Vector3.SmoothDamp(transform.position, center, ref velocity, SmoothTime);
+
+    CameraTarget.FieldOfView = Mathf.Lerp(FoVRange.x, FoVRange.y, width / MaxSpreadDistance);
+  }
+
+  Bounds? GetTargetBounds() {
+    bool found = false;
+    var bounds = new Bounds();
+    for (var i = 0; i < Targets.Count; i++) {
+      if (Targets[i] == null) continue;
+      var pos = Targets[i].position + TargetPositionBias;
+      if (!found) {
+        bounds = new Bounds(pos, Vector3.zero);
+        found = true;
+      } else {
+        bounds.Encapsulate(pos);
       }
-      count++;
-      sum += target.position;
-      min = Vector3.Min(min, target.position);
-      max = Vector3.Max(max, target.position);
     }
-
-
-    Vector3 targetPosition = TargetPositionBias + (count <= 0 ? Vector3.zero : sum / count);
-    Vector2 size = (Vector2) max - (Vector2) min;
-
-    // Calculate the actual padding to use
-    var actualPadding = new Vector2(1 + 2 * Padding.x, 1 + 2 * Padding.y);
-
-    // Compute Hadamard product between size and inverse padding to add the padding desired.
-    size = new Vector2(size.x * actualPadding.x, size.y * actualPadding.y);
-
-    // Calculate the target field of view for the proper cpuLevel of zoom
-    float targetFOV = 2f * Mathf.Atan(size.x * 0.5f / Mathf.Abs(transform.position.z - targetPosition.z))
-        * Mathf.Rad2Deg;
-
-    // Clamp the FOV so it isn't too small or too big.
-    targetFOV = Mathf.Clamp(targetFOV, FoVRange.x, FoVRange.y);
-
-    // Keep the camera in the same Z plane.
-    targetPosition.z = transform.position.z;
-
-    // Lerp both the FOV and the position at the desired speeds
-    CameraTarget.FieldOfView = Mathf.Lerp(CameraTarget.FieldOfView, targetFOV, Time.deltaTime * CameraSpeed);
-    transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * CameraSpeed);
-}
+    return found ? bounds : (Bounds?)null;
+  }
 
   public void RegisterTarget(Transform target) => Targets.Add(target);
   public void UnregisterTarget(Transform target) => Targets.Remove(target);
