3f9cf3d6dbdd5824fa0d81690b2e8f0ceef95edc
LadyEbony <jgarc129@ucr.edu>
2018-07-27 02:32:32 -0700

    Addressed code style. Changed component references to public and are updated at Awake.

--- Assets/Code/src/Runtime/Match/Components/MatchEndUI.cs
+++ Assets/Code/src/Runtime/Match/Components/MatchEndUI.cs
@@ -7,28 +7,10 @@ namespace HouraiTeahouse.FantasyCrescendo.Matches {
 public class MatchEndUI : MonoBehaviour {
 
     // Component References
-    // If variables are null, look for them
     [Header("Component References")]
-    [SerializeField] private TextMeshProUGUI _textUI;
-    public TextMeshProUGUI TextUI
-    {
-        get
-        {
-            if (_textUI == null)
-                _textUI = GetComponentInChildren<TextMeshProUGUI>();
-            return _textUI;
-        }
-    }
-    [SerializeField] private AudioSource _audioPlayer;
-    public AudioSource AudioPlayer
-    {
-        get
-        {
-            if (_audioPlayer == null)
-                _audioPlayer = GetComponentInChildren<AudioSource>();
-            return _audioPlayer;
-        }
-    }
+    public TextMeshProUGUI TextUI;
+    public AudioSource AudioPlayer;
+
     [Header("Timings")]
     public float PostGameWait = 5f;
 
@@ -40,7 +22,10 @@ public class MatchEndUI : MonoBehaviour {
     /// Awake is called when the script instance is being loaded.
     /// </summary>
     void Awake() {
-        Mediator.Global.CreateUnityContext(this).Subscribe<MatchEndEvent>(OnMatchEnd);
+        Mediator.Global.CreateUnityContext(this)
+            .Subscribe<MatchEndEvent>(OnMatchEnd);
+        TextUI = GetComponentInChildren<TextMeshProUGUI>();
+        AudioPlayer = GetComponentInChildren<AudioSource>();
     }
 
     
@@ -48,15 +33,13 @@ public class MatchEndUI : MonoBehaviour {
         ObjectUtil.SetActive(TextUI.gameObject, true);
         if (evt.MatchConfig.Time > 0 && evt.MatchState.Time <= 0) {
             UpdateTextUI("TIME!", TimeAudioClip);
-        }
-        else {
+        } else {
             UpdateTextUI("GAME!", GameAudioClip);
         }
         await Task.Delay((int)(PostGameWait * 1000));
     }
 
-    void UpdateTextUI(string text, AudioClip audio)
-    {
+    void UpdateTextUI(string text, AudioClip audio) {
         // Update Text and play Audio
         TextUI.text = text;
         AudioPlayer.clip = audio;
