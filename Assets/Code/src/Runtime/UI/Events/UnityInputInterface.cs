56fd6c45a77b06d019fa8b7b49498ff74abf6782
James Liu <contact@jamessliu.com>
2018-08-02 02:54:10 -0700

    Resolve #238: Add controller support to menus

---
+++ Assets/Code/src/Runtime/UI/Events/UnityInputInterface.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Experimental.Input;
+using UnityEngine.Experimental.Input.Controls;
+
+namespace HouraiTeahouse.FantasyCrescendo {
+
+public class UnityInputInterface : BaseInput {
+
+  public override bool mousePresent => Mouse.current != null;
+  public override Vector2 mousePosition => Mouse.current?.position?.ReadValue() ?? Vector2.zero;
+  public override Vector2 mouseScrollDelta => Mouse.current?.scroll?.ReadValue() ?? Vector2.zero;
+
+  public override bool GetMouseButton(int button) => GetMouseButtonControl(button)?.isPressed ?? false;
+  public override bool GetMouseButtonDown(int button) => GetMouseButtonControl(button)?.wasJustPressed ?? false;
+  public override bool GetMouseButtonUp(int button) => GetMouseButtonControl(button)?.wasJustReleased ?? false;
+
+  public override bool GetButtonDown(string controlPath) {
+    foreach (var control in InputSystem.GetControls(controlPath)) {
+      var button = control as ButtonControl;
+      if (button != null && button.wasJustPressed) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public override float GetAxisRaw(string controlPath) {
+    float sum = 0.0f;
+    foreach (var control in InputSystem.GetControls(controlPath)) {
+      var axis = control as AxisControl;
+      if (axis == null) continue;
+      sum += axis.ReadValue();
+    }
+    return Mathf.Clamp(sum, -1.0f, 1.0f);
+  }
+
+  ButtonControl GetMouseButtonControl(int buttonId) {
+    switch (buttonId) {
+      case 0: return Mouse.current?.leftButton;
+      case 1: return Mouse.current?.rightButton;
+      case 2: return Mouse.current?.middleButton;
+      default: return null;
+    }
+  }
+
+}
+
+}
