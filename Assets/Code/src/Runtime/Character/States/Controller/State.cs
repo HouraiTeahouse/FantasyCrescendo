0a0b5a346cf9bb07ae95d9a4ee813af81e83de05
James Liu <contact@jamessliu.com>
2018-08-04 03:44:32 -0700

    Add preliminary code for StateMachineAssets

--- Assets/Code/src/Runtime/Character/States/Controller/State.cs
+++ Assets/Code/src/Runtime/Character/States/Controller/State.cs
@@ -32,7 +32,7 @@ public class State : IEntity, IStateView<PlayerState>,
   readonly List<Transition> _transitions;
   public ReadOnlyCollection<Transition> Transitions { get; }
 
-  protected State() {
+  internal State() {
     _transitions = new List<Transition>();
     Transitions = new ReadOnlyCollection<Transition>(_transitions);
   }
