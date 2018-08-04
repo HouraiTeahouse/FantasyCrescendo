62a49eb277e4cb62284a4689e6d76c4d12314b98
LadyEbony <jgarc129@ucr.edu>
2018-08-04 04:16:18 -0700

    Changed code to match framework that James sent.

--- Assets/Code/src/Editor/StateMachineBuilder/StateMachineBuilderWindow.cs
+++ Assets/Code/src/Editor/StateMachineBuilder/StateMachineBuilderWindow.cs
@@ -5,30 +5,28 @@ using UnityEditor;
 using System.Linq;
 using System.Reflection;
 
-namespace HouraiTeahouse {
-  public class CCBuilderWindow : EditorWindow {
+namespace HouraiTeahouse.FantasyCrescendo {
+  public class StateMachineBuilderWindow : EditorWindow {
     private const float UpperTabHeight = 20;
     private readonly Vector2 NodeSize = new Vector2(120, 50);
     private readonly Vector2 NodeTextSize = new Vector2(60, 20);
 
-    public bool initDone = false;
-    public CCBuilderScriptableObject CCBuilder;
-    public GUIStyle labelStyle;
+    private bool initDone = false;
+    private StateMachineAsset smAsset;
+    private GUIStyle labelStyle;
 
-    public Texture2D lineTexture;
-    public Texture linkFreeTexture;
-    public Texture linkFullTexture;
+    private Texture2D lineTexture;
 
-    public CCBuilderScriptableObject.CCNode LinkingCC = null;
+    private StateMachineAsset.StateMachineNode sourceNode = null;
 
-    [MenuItem("Window/Character State Builder")]
+    [MenuItem("Window/State Machine Builder Window")]
     static void Init() {
-      GetWindow(typeof(CCBuilderWindow)).Show();
+      GetWindow(typeof(StateMachineBuilderWindow)).Show();
     }
 
     private void OnEnable() {
       initDone = false;
-      LinkingCC = null;
+      sourceNode = null;
     }
 
     private void InitStyles() {
@@ -43,15 +41,12 @@ namespace HouraiTeahouse {
       lineTexture.SetPixel(0, 0, new Color(0, 0, 0, 0.5f));
       lineTexture.SetPixel(0, 1, Color.white);
       lineTexture.Apply();
-
-      linkFreeTexture = Resources.Load("CCBuilder/LinkButtonFree") as Texture;
-      linkFullTexture = Resources.Load("CCBuilder/LinkButtonFull") as Texture;
     }
 
     private void OnGUI() {
       // Initialize
-      if (CCBuilder == null)
-        CCBuilder = CCBuilderScriptableObject.GetCCBuilder();
+      if (smAsset == null)
+        smAsset = StateMachineAsset.GetStateMachineAsset();
 
       if (!initDone)
         InitStyles();
@@ -62,7 +57,7 @@ namespace HouraiTeahouse {
       // Draw top bar buttons
       GUILayout.BeginHorizontal(GUILayout.MaxHeight(UpperTabHeight));
       if (GUILayout.Button("Add Node")) {
-        CCBuilder.AddNode();
+        smAsset.AddNode();
       }
       if (GUILayout.Button("Build")) {
         //CCBuilder.UpdateFile();
@@ -71,7 +66,7 @@ namespace HouraiTeahouse {
 
       // Draw each window
       BeginWindows();
-      foreach (var item in CCBuilder.NodeList) {
+      foreach (var item in smAsset.StateNodes) {
         item.Window = GUI.Window(item.Id, new Rect(item.Window.position, NodeSize), DrawNode, "", GUIStyle.none);
       }
       EndWindows();
@@ -79,30 +74,30 @@ namespace HouraiTeahouse {
       HandleLinkInput();
       DrawMouseLine();
 
-      EditorUtility.SetDirty(CCBuilder);
+      EditorUtility.SetDirty(smAsset);
     }
 
     private void OnInspectorUpdate() {
-      if (LinkingCC != null)
+      if (sourceNode != null)
         Repaint();
     }
 
     private void DrawNode(int id) {
-      var item = CCBuilder.NodeDictionary[id];
+      var node = smAsset.NodeDictionary[id];
       var e = Event.current;
 
       // Reposition so it doesn't get too out of screen
-      item.Window.position = new Vector2(Mathf.Max(item.Window.position.x, 0), Mathf.Max(item.Window.position.y, UpperTabHeight));
+      node.Window.position = new Vector2(Mathf.Max(node.Window.position.x, 0), Mathf.Max(node.Window.position.y, UpperTabHeight));
       
       // If touched, switch to that node's scriptable object
       if (e.button == 0 && e.type == EventType.MouseDown){
-        Selection.activeObject = CCBuilder;
+        Selection.activeObject = node.Asset;
       }
 
       // Draw the damn box and text
-      GUILayout.BeginArea(new Rect(Vector2.zero, item.Window.size), new GUIStyle("Box"));
-      var rect = new Rect((item.Window.size - NodeTextSize) / 2, NodeTextSize);
-      GUI.Label(rect, new GUIContent(item.Content), labelStyle);
+      GUILayout.BeginArea(new Rect(Vector2.zero, node.Window.size), new GUIStyle("Box"));
+      var rect = new Rect((node.Window.size - NodeTextSize) / 2, NodeTextSize);
+      GUI.Label(rect, new GUIContent(node.Asset.State), labelStyle);
       GUILayout.EndArea();
 
       // If right clicking, don't even try to drag
@@ -111,11 +106,11 @@ namespace HouraiTeahouse {
       }
     }
 
-    private void TryToConnectTwoNodes(CCBuilderScriptableObject.CCNode incomingLink) {
-      if (incomingLink != null && LinkingCC != incomingLink && !LinkingCC.Links.Contains(incomingLink.Id)) {
-        LinkingCC.AddLink(incomingLink.Id);
+    private void TryToConnectTwoNodes(StateMachineAsset.StateMachineNode destinationNode) {
+      if (destinationNode != null && sourceNode != destinationNode && !sourceNode.TransitionExists(destinationNode)) {
+        sourceNode.AddTransition(destinationNode);
       }
-      LinkingCC = null;
+      sourceNode = null;
       Repaint();
     }
 
@@ -124,17 +119,17 @@ namespace HouraiTeahouse {
       if (e.button == 1) {
         switch (e.type) {
           case EventType.MouseDown:
-            foreach (var item in CCBuilder.NodeList) {
+            foreach (var item in smAsset.StateNodes) {
               if (item.Window.Contains(e.mousePosition)) {
-                LinkingCC = item;
+                sourceNode = item;
                 break;
               }
             }
             break;
           case EventType.MouseUp:
-            if (LinkingCC == null) break;
-            CCBuilderScriptableObject.CCNode temp = null;
-            foreach (var item in CCBuilder.NodeList) {
+            if (sourceNode == null) break;
+            StateMachineAsset.StateMachineNode temp = null;
+            foreach (var item in smAsset.StateNodes) {
               if (item.Window.Contains(e.mousePosition)) {
                 temp = item;
                 break;
@@ -150,9 +145,9 @@ namespace HouraiTeahouse {
       Handles.BeginGUI();
       Handles.color = Color.black;
 
-      if (LinkingCC != null) {
-        var direction = (Event.current.mousePosition - LinkingCC.GetCenter).normalized;
-        Handles.DrawAAPolyLine(lineTexture, 3, LinkingCC.GetDrawLineEnd(direction), Event.current.mousePosition);
+      if (sourceNode != null) {
+        var direction = (Event.current.mousePosition - sourceNode.GetCenter).normalized;
+        Handles.DrawAAPolyLine(lineTexture, 3, sourceNode.GetDrawLineEnd(direction), Event.current.mousePosition);
       }
 
       Handles.EndGUI();
@@ -162,15 +157,15 @@ namespace HouraiTeahouse {
       Handles.BeginGUI();
       Handles.color = Color.black;
 
-      foreach (var item in CCBuilder.NodeList) {
-        foreach (var link in item.Links.Select(c => CCBuilder.NodeDictionary[c])) {
-          var direction = (link.GetCenter - item.GetCenter).normalized;
-          var startPoint = item.GetDrawLineEnd(direction);
-          var endPoint = link.GetDrawLineEnd(direction);
+      foreach (var node in smAsset.StateNodes) {
+        foreach (var transition in node.TransitionIds.Select(t => smAsset.NodeDictionary[t])) {
+          var direction = (transition.GetCenter - node.GetCenter).normalized;
+          var startPoint = node.GetDrawLineEnd(direction);
+          var endPoint = transition.GetDrawLineEnd(direction);
           var midPoint = (endPoint + startPoint) / 2;
           Handles.DrawAAPolyLine(lineTexture, 2, startPoint, endPoint);
-          Handles.DrawAAPolyLine(lineTexture, 2, item.GetDrawLineArrowEnd(direction, midPoint, Vector3.forward), midPoint,
-                                                  item.GetDrawLineArrowEnd(direction, midPoint, Vector3.back));
+          Handles.DrawAAPolyLine(lineTexture, 2, node.GetDrawLineArrowEnd(direction, midPoint, Vector3.forward), midPoint,
+                                                  node.GetDrawLineArrowEnd(direction, midPoint, Vector3.back));
         }
       }
 
