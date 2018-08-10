02cbf2907c1ae4f8b1e0fd62c4f572417083720d
James Liu <contact@jamessliu.com>
2018-08-10 04:13:08 -0700

    Add Custom Editor for StateTransitions

--- Assets/Code/src/Editor/StateMachine/BaseStateAssetEditor.cs
+++ Assets/Code/src/Editor/StateMachine/BaseStateAssetEditor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditorInternal;
 
 namespace HouraiTeahouse.FantasyCrescendo.Characters {
 
@@ -9,8 +10,31 @@ namespace HouraiTeahouse.FantasyCrescendo.Characters {
 [CanEditMultipleObjects]
 public class BaseStateAssetEditor : Editor {
 
+  const float kMuteButtonWidth = 50f;
+
+  ReorderableList _transitionList;
+  Dictionary<Object, Editor> _transitionEditors;
+
+  /// <summary>
+  /// This function is called when the object becomes enabled and active.
+  /// </summary>
+  void OnEnable() {
+    _transitionEditors = new Dictionary<Object, Editor>();
+
+    _transitionList = new ReorderableList(
+      serializedObject,
+      elements: serializedObject.FindProperty("_transitions"),
+      draggable: true,
+      displayHeader: true,
+      displayAddButton: false,
+      displayRemoveButton: true
+    );
+    _transitionList.drawHeaderCallback = OnDrawTransitionHeader;
+    _transitionList.drawElementCallback = OnDrawTransitionGUI;
+    _transitionList.onRemoveCallback = OnRemoveTransition;
+  }
+
   public override void OnInspectorGUI() {
-    // EditorGUILayout.PropertyField(serializedObject.FindProperty("StateType"));
     EditorGUI.BeginChangeCheck();
     OnStateGUI();
     OnTransitionGUI();
@@ -23,8 +47,15 @@ public class BaseStateAssetEditor : Editor {
 
   void OnTransitionGUI() {
     EditorGUILayout.Space();
-    EditorGUILayout.LabelField("Transitions", EditorStyles.boldLabel);
-    EditorGUILayout.PropertyField(serializedObject.FindProperty("_transitions"), true);
+    _transitionList.DoLayoutList();
+    EditorGUILayout.Space();
+    if (_transitionList.count <= 0) return;
+    if (_transitionList.index < 0) _transitionList.index = 0;
+    var property = GetSelectedProperty(_transitionList);
+    var editor = GetEditor(property.objectReferenceValue);
+    editor.DrawHeader();
+    EditorGUILayout.Space();
+    editor.OnInspectorGUI();
   }
 
   protected sealed override void OnHeaderGUI() {
@@ -40,6 +71,55 @@ public class BaseStateAssetEditor : Editor {
   }
 
   protected virtual void OnCustomHeaderGUI() { }
+
+  void OnDrawTransitionHeader(Rect rect) {
+    rect.width -= kMuteButtonWidth;
+    EditorGUI.LabelField(rect, "Transitions");
+    rect.x += rect.width;
+    rect.width = kMuteButtonWidth;
+    EditorGUI.LabelField(rect, "Muted");
+  }
+
+  void OnDrawTransitionGUI(Rect rect, int index, bool isActive, bool isFocused) {
+    var element = _transitionList.serializedProperty.GetArrayElementAtIndex(index);
+    var editor = GetEditor(element.objectReferenceValue);
+    var transition = element.objectReferenceValue as StateTransitionAsset;
+    rect.width -= kMuteButtonWidth;
+    EditorGUI.LabelField(rect, transition.DisplayName);
+    rect.x += rect.width;
+    rect.width = kMuteButtonWidth;
+    EditorGUI.BeginChangeCheck();
+    EditorGUI.PropertyField(rect, editor.serializedObject.FindProperty("Muted"), GUIContent.none);
+    if (EditorGUI.EndChangeCheck()) {
+      editor.serializedObject.ApplyModifiedProperties();
+    }
+  }
+
+  void OnRemoveTransition(ReorderableList list) {
+    var property = list.serializedProperty;
+    var element = GetSelectedProperty(list);
+    var transition = element.objectReferenceValue as StateTransitionAsset;
+    _transitionEditors.Remove(element.objectReferenceValue);
+    if (transition != null) {
+      transition.Destroy();
+    }
+    // Needs to delete twice to remove it from the list
+    property.DeleteArrayElementAtIndex(list.index);
+    property.DeleteArrayElementAtIndex(list.index);
+  }
+
+  SerializedProperty GetSelectedProperty(ReorderableList list) {
+    return list.serializedProperty.GetArrayElementAtIndex(list.index);
+  }
+
+  Editor GetEditor(Object obj) {
+    Editor editor;
+    if (!_transitionEditors.TryGetValue(obj, out editor)) {
+      editor = Editor.CreateEditor(obj);
+      _transitionEditors[obj] = editor;
+    }
+    return editor;
+  }
 
 }
 
---
+++ Assets/Code/src/Editor/StateMachine/StateTransitionAssetEditor.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+
+namespace HouraiTeahouse.FantasyCrescendo.Characters {
+
+[CustomEditor(typeof(StateTransitionAsset))]
+public class StateTransitionAssetEditor : Editor {
+
+  ReorderableList _conditionList;
+  GUIStyle _enabledToggle;
+
+  /// <summary>
+  /// This function is called when the object becomes enabled and active.
+  /// </summary>
+  void OnEnable() {
+    _conditionList = new ReorderableList(
+      serializedObject,
+      elements: serializedObject.FindProperty("_conditions"),
+      draggable: true,
+      displayHeader: true,
+      displayAddButton: true,
+      displayRemoveButton: true
+    );
+    _conditionList.drawHeaderCallback = OnConditionHeaderGUI;
+    _conditionList.drawElementCallback = OnDrawConditionGUI;
+
+    _enabledToggle = new GUIStyle(EditorStyles.toolbarButton);
+    _enabledToggle.normal.background = _enabledToggle.active.background;
+  }
+
+  public override void OnInspectorGUI() {
+    EditorGUI.BeginChangeCheck();
+    EditorGUILayout.PropertyField(serializedObject.FindProperty("TransitionRequirement"));
+    EditorGUILayout.Space();
+    _conditionList.DoLayoutList();
+    if (EditorGUI.EndChangeCheck()) {
+      serializedObject.ApplyModifiedProperties();
+    }
+  }
+
+  protected sealed override void OnHeaderGUI() {
+    var transition = target as StateTransitionAsset;
+    var name = serializedObject.FindProperty("m_Name");
+    var muted = serializedObject.FindProperty("Muted");
+
+    EditorGUI.BeginChangeCheck();
+    EditorGUILayout.BeginVertical(EditorStyles.toolbar);
+    EditorGUILayout.BeginHorizontal();
+    var nameText = EditorGUILayout.TextField(transition.DisplayName, EditorStyles.toolbarTextField, GUILayout.ExpandWidth(true));
+    name.stringValue = nameText != transition.DefaultName ? nameText : string.Empty;
+    if (GUILayout.Button("Muted", muted.boolValue ? _enabledToggle : EditorStyles.toolbarButton, GUILayout.MaxWidth(50))) {
+      muted.boolValue = !muted.boolValue;
+    }
+    EditorGUILayout.EndHorizontal();
+    EditorGUILayout.EndVertical();
+    if (EditorGUI.EndChangeCheck()) {
+      serializedObject.ApplyModifiedProperties();
+    }
+  }
+
+  void OnConditionHeaderGUI(Rect rect) => EditorGUI.LabelField(rect, "Conditions");
+
+  void OnDrawConditionGUI(Rect rect, int index, bool isActive, bool isFocused) {
+    var element = _conditionList.serializedProperty.GetArrayElementAtIndex(index);
+    var typeProperty = element.FindPropertyRelative("Type");
+    EditorGUI.PropertyField(GetSubRect(rect, 0f, 1/3f), typeProperty, GUIContent.none);
+    switch (GetConditionType(typeProperty)) {
+      case StateTransitionCondition.ConditionType.InputDirection: 
+        var axis = element.FindPropertyRelative("Axis");
+        var direction = element.FindPropertyRelative("AxisDirection");
+        EditorGUI.PropertyField(GetSubRect(rect, 1/3f, 1/3f), axis, GUIContent.none);
+        EditorGUI.PropertyField(GetSubRect(rect, 2/3f, 1/3f), direction, GUIContent.none);
+        break;
+      default:
+        var subproperty = GetConditionSubProperty(element, typeProperty);
+        EditorGUI.PropertyField(GetSubRect(rect, 1/3f, 2/3f), subproperty, GUIContent.none);
+        break;
+    }
+  }
+
+  StateTransitionCondition.ConditionType GetConditionType(SerializedProperty property) {
+    return (StateTransitionCondition.ConditionType)property.intValue;
+  }
+
+  SerializedProperty GetConditionSubProperty(SerializedProperty root, SerializedProperty type) {
+    switch (GetConditionType(type)) {
+      case StateTransitionCondition.ConditionType.InputButtonWasPressed: 
+      case StateTransitionCondition.ConditionType.InputButtonWasReleased: 
+      case StateTransitionCondition.ConditionType.InputButtonHeld:
+        return root.FindPropertyRelative("Button");
+      case StateTransitionCondition.ConditionType.InputDirection: 
+        return root.FindPropertyRelative("Axis");
+      case StateTransitionCondition.ConditionType.IsGrounded:
+      case StateTransitionCondition. ConditionType.IsHit:
+      case StateTransitionCondition.ConditionType.IsGrabbingLedge:
+      case StateTransitionCondition.ConditionType.CanJump:
+      case StateTransitionCondition.ConditionType.IsShieldBroken:
+      case  StateTransitionCondition.ConditionType.PlayerDirection:
+        return root.FindPropertyRelative("BoolParam");
+      case StateTransitionCondition.ConditionType.Time:
+        return root.FindPropertyRelative("FloatParam");
+      default: return null;
+    }
+  }
+
+  Rect GetSubRect(Rect original, float start, float length) {
+    original.x += start * original.width;
+    original.width = length * original.width;
+    return original;
+  }
+
+}
+
+}
